2f252ba1f0056f124992eab16032365e82a77dac
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-05-21 17:30:52 +0100

    initial framework

---
+++ src/main/java/net/kenro/ji/jin/purescript/PSIcons.java
@@ -0,0 +1,9 @@
+package net.kenro.ji.jin.purescript;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface PSIcons {
+    Icon FILE = IconLoader.getIcon("/icons/pure.png");
+}
