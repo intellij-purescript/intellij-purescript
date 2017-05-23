21c3e34931182c8831779b02b6c8bd18d1f75400
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-05-23 14:48:32 +0100

    more work on parser / lexer framework

---
+++ del_expected.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -rf src/test/resources/gold/parser/expected/*.txt
---
+++ replace_expected.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf src/test/resources/gold/parser/expected/*.txt
+cp build/resources/test/gold/parser/expected/*.txt src/test/resources/gold/parser/expected
