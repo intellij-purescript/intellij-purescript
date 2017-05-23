d42e59c9a400674d0a1e710fe242827320909559
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-05-23 14:21:02 +0100

    more work on parser / lexer framework

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -rf build
+rm -rf src/main/gen
+
+#GRAMMAR_KIT="/Users/hendrkin/Library/Application Support/IntelliJIdea2017.1/GrammarKit/lib/grammar-kit.jar"
+#LIB="/Applications/IntelliJ IDEA.app/Contents/lib/"
+#GEN_DIR=src/main/gen
+#BNF_FILE=src/main/java/net/kenro/ji/jin/purescript/grammar/Purescript.bnf
+#java -cp "$GRAMMAR_KIT:$LIB/*:src" org.intellij.grammar.Main $GEN_DIR $BNF_FILE
