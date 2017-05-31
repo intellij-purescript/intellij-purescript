d63c24fe908dc19e6ff333d6d4f96951a66b3143
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-05-31 18:01:32 +0100

    more work

--- src/test/java/net/kenro/ji/jin/purescript/parser/PSLanguageParserTest.java
+++ src/test/java/net/kenro/ji/jin/purescript/parser/PSLanguageParserTest.java
@@ -75,59 +75,59 @@ public class PSLanguageParserTest extends PSLanguageParserTestBase {
     public void testPolyLabels() { doTest(true, true); }
     public void testQualifiedQualifiedImports() { doTest(true, true); }
     public void testDollar() { doTest(true, true); }
+    public void testConstraintParens() { doTest(true, true); }
+    public void testConstraintParsingIssue() { doTest(true, true); }
+    public void testDerivingFunctor() { doTest(true, true); }
+    public void testFunctionalDependencies() { doTest(true, true); }
+    public void testGenericsRep() { doTest(true, true); }
+    public void testIfWildcard() { doTest(true, true); }
+    public void testMPTCs() { doTest(true, true); }
+    public void testMonadState() { doTest(true, true); }
+    public void testNewtypeClass() { doTest(true, true); }
+    public void testOperatorAlias() { doTest(true, true); }
+    public void testOperatorAliasElsewhere() { doTest(true, true); }
+    public void testOperators() { doTest(true, true); }
+    public void testRebindableSyntax() { doTest(true, true); }
+    public void testRowInInstanceHeadDetermined() { doTest(true, true); }
+    public void testRowPolyInstanceContext() { doTest(true, true); }
+    public void testRowsInInstanceContext() { doTest(true, true); }
+    public void testSuperclasses3() { doTest(true, true); }
+    public void testUnicodeType() { doTest(true, true); }
+    public void testUntupledConstraints() { doTest(true, true); }
+    public void testUsableTypeClassMethods() { doTest(true, true); }
+    public void testWildcardInInstance() { doTest(true, true); }
+    public void testTypeClasses() { doTest(true, true); }
+    public void testTypedBinders() { doTest(true, true); }
 
-    //public void test2663() { doTest(true, true); }
+//    public void testDctorOperatorAlias() { doTest(true, true); }
+//    public void test2663() { doTest(true, true); }
 //    public void test2378() { doTest(true, true); }
 //    public void test2049_named_pattern_matching() { doTest(true, true); }
-//    public void testConstraintParens() { doTest(true, true); }
-//    public void testConstraintParsingIssue() { doTest(true, true); }
-//    public void testDctorOperatorAlias() { doTest(true, true); }
-
-//    public void testDerivingFunctor() { doTest(true, true); }
 //    public void testDuplicateProperties() { doTest(true, true); }
 //    public void testExtendedInfixOperators() { doTest(true, true); }
 //    public void testFieldConsPuns() { doTest(true, true); }
 //    public void testFieldPuns() { doTest(true, true); }
 //    public void testForeignKind() { doTest(true, true); }
 //    public void testFunWithFunDeps() { doTest(true, true); }
-//    public void testFunctionalDependencies() { doTest(true, true); }
-//    public void testGenericsRep() { doTest(true, true); }
 //    public void testGuards() { doTest(true, true); }
-//    public void testIfWildcard() { doTest(true, true); }
 //    public void testIntAndChar() { doTest(true, true); }
 //    public void testKindedType() { doTest(true, true); }
 //    public void testLetPattern() { doTest(true, true); }
-//    public void testMPTCs() { doTest(true, true); }
-//    public void testMonadState() { doTest(true, true); }
 //    public void testMutRec2() { doTest(true, true); }
 //    public void testMutRec3() { doTest(true, true); }
 //    public void testNestedRecordUpdate() { doTest(true, true); }
 //    public void testNestedRecordUpdateWildcards() { doTest(true, true); }
-//    public void testNewtypeClass() { doTest(true, true); }
 //    public void testNewtypeInstance() { doTest(true, true); }
-//    public void testOperatorAlias() { doTest(true, true); }
-//    public void testOperatorAliasElsewhere() { doTest(true, true); }
-//    public void testOperators() { doTest(true, true); }
 //    public void testPrimedTypeName() { doTest(true, true); }
-//    public void testRebindableSyntax() { doTest(true, true); }
 //    public void testRowConstructors() { doTest(true, true); }
-//    public void testRowInInstanceHeadDetermined() { doTest(true, true); }
-//    public void testRowPolyInstanceContext() { doTest(true, true); }
 //    public void testRowUnion() { doTest(true, true); }
-//    public void testRowsInInstanceContext() { doTest(true, true); }
 //    public void testSolvingAppendSymbol() { doTest(true, true); }
 //    public void testSolvingCompareSymbol() { doTest(true, true); }
 //    public void testStream() { doTest(true, true); }
-//    public void testSuperclasses3() { doTest(true, true); }
 //    public void testTailCall() { doTest(true, true); }
-//    public void testTypeClasses() { doTest(true, true); }
-    public void testTypeOperators() { doTest(true, true); }
-    public void testTypedBinders() { doTest(true, true); }
+//    public void testTypeOperators() { doTest(true, true); }
 //    public void testUnicodeOperators() { doTest(true, true); }
-    public void testUnicodeType() { doTest(true, true); }
-    public void testUntupledConstraints() { doTest(true, true); }
-    public void testUsableTypeClassMethods() { doTest(true, true); }
-    public void testWildcardInInstance() { doTest(true, true); }
+
 
 
 
