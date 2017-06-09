3ff9ae1e0cd09cc089264739e26a1d80d7b26cf3
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-06-09 07:01:41 +0100

    added commenter

---
+++ src/main/java/net/kenro/ji/jin/purescript/features/PSCommenter.java
@@ -0,0 +1,37 @@
+package net.kenro.ji.jin.purescript.features;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class PSCommenter implements Commenter {
+
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "--";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return "{-";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return "-}";
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return "{-";
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return "-}";
+    }
+}
