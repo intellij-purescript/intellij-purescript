9c1556821f82a1a881c57129651ff4ed5c3fd12a
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-06-12 13:21:39 +0100

    more work on references

--- src/main/java/net/kenro/ji/jin/purescript/file/PSFile.java
+++ src/main/java/net/kenro/ji/jin/purescript/file/PSFile.java
@@ -3,10 +3,14 @@ package net.kenro.ji.jin.purescript.file;
 import com.intellij.extapi.psi.PsiFileBase;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.util.PsiTreeUtil;
 import net.kenro.ji.jin.purescript.PSLanguage;
+import net.kenro.ji.jin.purescript.psi.PSImportDeclaration;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class PSFile extends PsiFileBase {
     public PSFile(@NotNull FileViewProvider viewProvider) {
@@ -27,5 +31,9 @@ public class PSFile extends PsiFileBase {
     @Override
     public Icon getIcon(int flags) {
         return super.getIcon(flags);
+    }
+
+    public List<PSImportDeclaration> getImportClauses() {
+        return new LinkedList<>(PsiTreeUtil.findChildrenOfType(this, PSImportDeclaration.class));
     }
 }
--- src/main/java/net/kenro/ji/jin/purescript/psi/scope/PSTypesProvider.java
+++ src/main/java/net/kenro/ji/jin/purescript/psi/scope/PSTypesProvider.java
@@ -1,101 +1,101 @@
-//package net.kenro.ji.jin.purescript.psi.scope;
-//
-//import net.kenro.ji.jin.purescript.file.PSFile;
-//import net.kenro.ji.jin.purescript.psi.PSImportDeclaration;
-//import net.kenro.ji.jin.purescript.psi.PSProperName;
-//import net.kenro.ji.jin.purescript.util.TypeFilter;
-//
-//import java.util.Optional;
-//import java.util.Stack;
-//
-//import static net.kenro.ji.jin.purescript.psi.scope.PSTypesProvider.TypesProvidingPhase.*;
-//
-//class PSTypesProvider {
-//    private final PSFile file;
-//
-//    private final Stack<PSProperName> types = new Stack<>();
-//    private final Stack<PSImportDeclaration> importClauses = new Stack<>();
-//    private final Stack<String> implicitImports;
-//    private TypesProvidingPhase phase = CURRENT_FILE;
-//
-//    ElmTypesProvider(PSFile file) {
-//        this.file = file;
-//        this.implicitImports = PSCoreLibrary.getImplicitImportsCopy();
-//    }
-//
-//    Optional<PSProperName> nextType() {
-//        if (!this.types.isEmpty()) {
-//            return Optional.of(types.pop());
-//        }
-//
-//        switch (this.phase) {
-//            case CURRENT_FILE:
-//                gatherTypesFromCurrentFile();
+package net.kenro.ji.jin.purescript.psi.scope;
+
+import net.kenro.ji.jin.purescript.file.PSFile;
+import net.kenro.ji.jin.purescript.psi.PSImportDeclaration;
+import net.kenro.ji.jin.purescript.psi.PSProperName;
+import net.kenro.ji.jin.purescript.util.TypeFilter;
+
+import java.util.Optional;
+import java.util.Stack;
+
+import static net.kenro.ji.jin.purescript.psi.scope.PSTypesProvider.TypesProvidingPhase.*;
+
+class PSTypesProvider {
+    private final PSFile file;
+
+    private final Stack<PSProperName> types = new Stack<>();
+    private final Stack<PSImportDeclaration> importClauses = new Stack<>();
+    private final Stack<String> implicitImports;
+    private TypesProvidingPhase phase = CURRENT_FILE;
+
+    PSTypesProvider(PSFile file) {
+        this.file = file;
+        this.implicitImports = PSCoreLibrary.getImplicitImportsCopy();
+    }
+
+    Optional<PSProperName> nextType() {
+        if (!this.types.isEmpty()) {
+            return Optional.of(types.pop());
+        }
+
+        switch (this.phase) {
+            case CURRENT_FILE:
+                gatherTypesFromCurrentFile();
+                return this.nextType();
+//            case IMPORTED_FILES:
+//                gatherTypesFromImport();
 //                return this.nextType();
-////            case IMPORTED_FILES:
-////                gatherTypesFromImport();
-////                return this.nextType();
-////            case IMPLICIT_IMPORTS:
-////                gatherTypesFromImplicitImport();
-////                return this.nextType();
-//            case FINISHED:
-//                return Optional.empty();
-//            default:
-//                throw new RuntimeException("Unhandled phase " + this.phase);
-//        }
-//    }
-//
-//    private void gatherTypesFromCurrentFile() {
+//            case IMPLICIT_IMPORTS:
+//                gatherTypesFromImplicitImport();
+//                return this.nextType();
+            case FINISHED:
+                return Optional.empty();
+            default:
+                throw new RuntimeException("Unhandled phase " + this.phase);
+        }
+    }
+
+    private void gatherTypesFromCurrentFile() {
 //        this.file.getInternalTypes()
 //                .forEach(this.types::push);
-//        this.file.getImportClauses()
-//                .forEach(this.importClauses::push);
-//        this.updatePhase();
+        this.file.getImportClauses()
+                .forEach(this.importClauses::push);
+        this.updatePhase();
+    }
+
+//    private void gatherTypesFromImport() {
+//        ElmImportClause importClause = this.importClauses.pop();
+//        Optional.ofNullable(importClause.getExposingClause())
+//                .ifPresent(exposingClause -> gatherTypesFromExposingClause(importClause.getModuleName().getText(), exposingClause));
+//        updatePhase();
+//    }
+
+//    private void gatherTypesFromExposingClause(String moduleName, ElmExposingClause exposingClause) {
+//        TypeFilter filter = exposingClause.isExposingAll()
+//                ? TypeFilter.always(true)
+//                : exposingClause.getExposedTypeFilter();
+//        this.gatherTypesFromFile(moduleName, filter);
 //    }
 //
-////    private void gatherTypesFromImport() {
-////        ElmImportClause importClause = this.importClauses.pop();
-////        Optional.ofNullable(importClause.getExposingClause())
-////                .ifPresent(exposingClause -> gatherTypesFromExposingClause(importClause.getModuleName().getText(), exposingClause));
-////        updatePhase();
-////    }
-//
-////    private void gatherTypesFromExposingClause(String moduleName, ElmExposingClause exposingClause) {
-////        TypeFilter filter = exposingClause.isExposingAll()
-////                ? TypeFilter.always(true)
-////                : exposingClause.getExposedTypeFilter();
-////        this.gatherTypesFromFile(moduleName, filter);
-////    }
-////
-////    private void gatherTypesFromImplicitImport() {
-////        String module = this.implicitImports.pop();
-////        this.gatherTypesFromFile(module, TypeFilter.always(true));
-////        updatePhase();
-////    }
-////
-////    private void gatherTypesFromFile(String moduleName, TypeFilter filter) {
-////        ElmModuleIndex.getFilesByModuleName(moduleName, this.file.getProject())
-////                .forEach(f -> this.gatherTypesFromFile(f, filter));
-////    }
-////
-////    private void gatherTypesFromFile(PSFile file, TypeFilter filter) {
-////        file.getExposedTypes(filter)
-////                .forEach(this.types::push);
-////    }
-//
-//    private void updatePhase() {
-//        this.phase = this.implicitImports.isEmpty()
-//                ? FINISHED
-//                : this.importClauses.isEmpty()
-//                ? IMPLICIT_IMPORTS
-//                : IMPORTED_FILES;
+//    private void gatherTypesFromImplicitImport() {
+//        String module = this.implicitImports.pop();
+//        this.gatherTypesFromFile(module, TypeFilter.always(true));
+//        updatePhase();
 //    }
 //
-//    enum TypesProvidingPhase {
-//        CURRENT_FILE,
-//        IMPORTED_FILES,
-//        IMPLICIT_IMPORTS,
-//        FINISHED
+//    private void gatherTypesFromFile(String moduleName, TypeFilter filter) {
+//        ElmModuleIndex.getFilesByModuleName(moduleName, this.file.getProject())
+//                .forEach(f -> this.gatherTypesFromFile(f, filter));
 //    }
 //
-//}
+//    private void gatherTypesFromFile(PSFile file, TypeFilter filter) {
+//        file.getExposedTypes(filter)
+//                .forEach(this.types::push);
+//    }
+
+    private void updatePhase() {
+        this.phase = this.implicitImports.isEmpty()
+                ? FINISHED
+                : this.importClauses.isEmpty()
+                ? IMPLICIT_IMPORTS
+                : IMPORTED_FILES;
+    }
+
+    enum TypesProvidingPhase {
+        CURRENT_FILE,
+        IMPORTED_FILES,
+        IMPLICIT_IMPORTS,
+        FINISHED
+    }
+
+}
