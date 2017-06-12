1058592635b880a0e3556cfe4e7fb5481eda8360
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-06-12 09:18:59 +0100

    more work on references

--- src/main/java/net/kenro/ji/jin/purescript/psi/references/PSTypeReference.java
+++ src/main/java/net/kenro/ji/jin/purescript/psi/references/PSTypeReference.java
@@ -1,36 +1,36 @@
-package net.kenro.ji.jin.purescript.psi.references;
-
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import net.kenro.ji.jin.purescript.file.PSFile;
-import net.kenro.ji.jin.purescript.psi.PSProperName;
-import net.kenro.ji.jin.purescript.psi.scope.PSScope;
-import net.kenro.ji.jin.purescript.util.Function3;
-import org.jetbrains.annotations.Nullable;
-
-public class PSTypeReference extends PSReferenceBase<PSProperName> {
-    public PSTypeReference(PSProperName element) {
-        super(element);
-    }
-
-    private PSTypeReference(PsiElement element, PSProperName referencingElement, TextRange rangeInElement) {
-        super(element, referencingElement, rangeInElement);
-    }
-
-    @Override
-    protected Function3<PsiElement, PSProperName, TextRange, PSReference> constructor() {
-        return PSTypeReference::new;
-    }
-
-    @Nullable
-    @Override
-    public PsiElement resolve() {
-        PsiFile file = this.myElement.getContainingFile();
-        return PSScope.typesFor((PSFile) file)
-                .filter(this::theSameNameOrEmpty)
-                .findFirst()
-                .map(o -> o.orElse(null))
-                .orElse(null);
-    }
-}
+//package net.kenro.ji.jin.purescript.psi.references;
+//
+//import com.intellij.openapi.util.TextRange;
+//import com.intellij.psi.PsiElement;
+//import com.intellij.psi.PsiFile;
+//import net.kenro.ji.jin.purescript.file.PSFile;
+//import net.kenro.ji.jin.purescript.psi.PSProperName;
+//import net.kenro.ji.jin.purescript.psi.scope.PSScope;
+//import net.kenro.ji.jin.purescript.util.Function3;
+//import org.jetbrains.annotations.Nullable;
+//
+//public class PSTypeReference extends PSReferenceBase<PSProperName> {
+//    public PSTypeReference(PSProperName element) {
+//        super(element);
+//    }
+//
+//    private PSTypeReference(PsiElement element, PSProperName referencingElement, TextRange rangeInElement) {
+//        super(element, referencingElement, rangeInElement);
+//    }
+//
+//    @Override
+//    protected Function3<PsiElement, PSProperName, TextRange, PSReference> constructor() {
+//        return PSTypeReference::new;
+//    }
+//
+//    @Nullable
+//    @Override
+//    public PsiElement resolve() {
+//        PsiFile file = this.myElement.getContainingFile();
+//        return PSScope.typesFor((PSFile) file)
+//                .filter(this::theSameNameOrEmpty)
+//                .findFirst()
+//                .map(o -> o.orElse(null))
+//                .orElse(null);
+//    }
+//}
--- src/main/java/net/kenro/ji/jin/purescript/psi/references/PSValueReference.java
+++ src/main/java/net/kenro/ji/jin/purescript/psi/references/PSValueReference.java
@@ -1,34 +1,34 @@
-package net.kenro.ji.jin.purescript.psi.references;
-
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
-import net.kenro.ji.jin.purescript.psi.PSIdentifier;
-import net.kenro.ji.jin.purescript.psi.scope.PSScope;
-import net.kenro.ji.jin.purescript.util.Function3;
-import org.jetbrains.annotations.Nullable;
-
-public class PSValueReference extends PSReferenceBase<PSIdentifier> {
-
-    public PSValueReference(PSIdentifier element) {
-        super(element);
-    }
-
-    private PSValueReference(PsiElement element, PSIdentifier referencingElement, TextRange rangeInElement) {
-        super(element, referencingElement, rangeInElement);
-    }
-
-    @Override
-    protected Function3<PsiElement, PSIdentifier, TextRange, PSReference> constructor() {
-        return PSValueReference::new;
-    }
-
-    @Nullable
-    @Override
-    public PsiElement resolve() {
-        return PSScope.scopeFor(this.referencingElement)
-                .filter(this::theSameNameOrEmpty)
-                .findFirst()
-                .map(o -> o.orElse(null))
-                .orElse(null);
-    }
-}
+//package net.kenro.ji.jin.purescript.psi.references;
+//
+//import com.intellij.openapi.util.TextRange;
+//import com.intellij.psi.PsiElement;
+//import net.kenro.ji.jin.purescript.psi.PSIdentifier;
+//import net.kenro.ji.jin.purescript.psi.scope.PSScope;
+//import net.kenro.ji.jin.purescript.util.Function3;
+//import org.jetbrains.annotations.Nullable;
+//
+//public class PSValueReference extends PSReferenceBase<PSIdentifier> {
+//
+//    public PSValueReference(PSIdentifier element) {
+//        super(element);
+//    }
+//
+//    private PSValueReference(PsiElement element, PSIdentifier referencingElement, TextRange rangeInElement) {
+//        super(element, referencingElement, rangeInElement);
+//    }
+//
+//    @Override
+//    protected Function3<PsiElement, PSIdentifier, TextRange, PSReference> constructor() {
+//        return PSValueReference::new;
+//    }
+//
+//    @Nullable
+//    @Override
+//    public PsiElement resolve() {
+//        return PSScope.scopeFor(this.referencingElement)
+//                .filter(this::theSameNameOrEmpty)
+//                .findFirst()
+//                .map(o -> o.orElse(null))
+//                .orElse(null);
+//    }
+//}
