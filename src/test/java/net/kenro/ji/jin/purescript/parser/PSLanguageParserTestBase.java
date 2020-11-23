a34b895632088e656dfea688c01f4dffaf624c9f
Samuel Ytterbrink <neppord@gmail.com>
2020-11-23 10:51:44 +0100

    Reformat file before editing

--- src/test/java/net/kenro/ji/jin/purescript/parser/PSLanguageParserTestBase.java
+++ src/test/java/net/kenro/ji/jin/purescript/parser/PSLanguageParserTestBase.java
@@ -14,7 +14,11 @@ import java.io.IOException;
 
 public abstract class PSLanguageParserTestBase extends ParsingTestCase {
 
-    public PSLanguageParserTestBase(String dataPath, String fileExt, ParserDefinition... definitions) {
+    public PSLanguageParserTestBase(
+        String dataPath,
+        String fileExt,
+        ParserDefinition... definitions
+    ) {
         super(dataPath, fileExt, definitions);
     }
 
@@ -36,25 +40,29 @@ public abstract class PSLanguageParserTestBase extends ParsingTestCase {
         doTest(true);
         if (shouldPass) {
             assertFalse(
-                    "PsiFile contains error elements",
-                    toParseTreeText(myFile, skipSpaces(), includeRanges()).contains("PsiErrorElement")
+                "PsiFile contains error elements",
+                toParseTreeText(myFile, skipSpaces(), includeRanges()).contains(
+                    "PsiErrorElement")
             );
         }
     }
 
 
     @Override
-    protected void checkResult(@NonNls @TestDataFile String targetDataName,
-                               final PsiFile file) throws IOException {
+    protected void checkResult(
+        @NonNls @TestDataFile String targetDataName,
+        final PsiFile file
+    ) throws IOException {
         doCheckResult(myFullDataPath, file, checkAllPsiRoots(),
-                "" + File.separator + targetDataName, skipSpaces(),
-                includeRanges());
+            "" + File.separator + targetDataName, skipSpaces(),
+            includeRanges()
+        );
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        VirtualFile m = new MockVirtualFile(true,myFullDataPath);
+        VirtualFile m = new MockVirtualFile(true, myFullDataPath);
         myProject.setBaseDir(m);
     }
 
