608a47177a68759476679e4b5ddccfd78a32f24f
Samuel Ytterbrink <neppord@gmail.com>
2020-11-24 10:29:27 +0100

    Remove sh buildscripts that are replaced with gradle and ide

--- generate_parser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-GEN_DIR=src/main/gen
-JFLEX="jflex-1.7.0-SNAPSHOT.jar"
-FLEX_FILE=src/main/grammar/Purescript.flex
-SKEL_FILE=src/main/grammar/idea-flex.skeleton
-java -cp "$JFLEX" -jar $JFLEX -d $GEN_DIR --skel $SKEL_FILE $FLEX_FILE
