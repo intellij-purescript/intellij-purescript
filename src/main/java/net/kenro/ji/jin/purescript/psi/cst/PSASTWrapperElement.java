745d8e48a82c80e233d4b578eb247072d6f76470
Samuel Ytterbrink <neppord@gmail.com>
2020-11-26 08:07:07 +0100

    Remove language injection
    That is dependent on the java plugin

--- src/main/java/net/kenro/ji/jin/purescript/psi/cst/PSASTWrapperElement.java
+++ src/main/java/net/kenro/ji/jin/purescript/psi/cst/PSASTWrapperElement.java
@@ -2,13 +2,12 @@ package net.kenro.ji.jin.purescript.psi.cst;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.impl.source.tree.LeafElement;
 import com.intellij.psi.tree.IElementType;
 import net.kenro.ji.jin.purescript.psi.PSElements;
 import org.jetbrains.annotations.NotNull;
 
-public class PSASTWrapperElement extends ASTWrapperPsiElement implements PsiLanguageInjectionHost {
+public class PSASTWrapperElement extends ASTWrapperPsiElement {
     public PSASTWrapperElement(final ASTNode astNode) {
         super(astNode);
     }
@@ -34,23 +33,10 @@ public class PSASTWrapperElement extends ASTWrapperPsiElement implements PsiLang
         return isString() ? getNode().getFirstChildNode().getText() : "";
     }
 
-    @Override
-    public boolean isValidHost() {
-        // Only supports block-strings or single-line strings.
-        return isString() && (isBlockString() || !isMultilineString());
-    }
-
-    @Override
     public PSASTWrapperElement updateText(@NotNull final String s) {
         final ASTNode valueNode = getNode().getFirstChildNode();
         assert valueNode instanceof LeafElement;
-        ((LeafElement)valueNode).replaceWithText(s);
+        ((LeafElement) valueNode).replaceWithText(s);
         return this;
-    }
-
-    @NotNull
-    @Override
-    public PSStringLiteralEscaper createLiteralTextEscaper() {
-        return new PSStringLiteralEscaper(this);
     }
 }
