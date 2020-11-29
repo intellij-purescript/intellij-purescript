8d938848b66d95704a7aa1211cb83622b0f783ae
Samuel Ytterbrink <neppord@gmail.com>
2020-11-29 21:56:44 +0100

    Find top level value declarations on go to declaration

---
+++ src/main/java/net/kenro/ji/jin/purescript/features/PSFindUsageProvider.java
@@ -0,0 +1,51 @@
+package net.kenro.ji.jin.purescript.features;
+
+import com.intellij.lang.findUsages.FindUsagesProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import net.kenro.ji.jin.purescript.psi.impl.PSValueDeclarationImpl;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PSFindUsageProvider implements FindUsagesProvider {
+    @Override
+    public boolean canFindUsagesFor(@NotNull final PsiElement psiElement) {
+        return psiElement instanceof PSValueDeclarationImpl;
+    }
+
+    @Override
+    public @Nullable String getHelpId(@NotNull final PsiElement psiElement) {
+        return null;
+    }
+
+    @Override
+    public @Nls @NotNull String getType(@NotNull final PsiElement element) {
+        if (element instanceof PSValueDeclarationImpl) {
+            return "value";
+        }
+        return "unknown";
+    }
+
+    @Override
+    public @Nls @NotNull String getDescriptiveName(@NotNull final PsiElement element) {
+        if (element instanceof PsiNamedElement) {
+            final String name = ((PsiNamedElement) element).getName();
+            if (name != null) {
+                return name;
+            }
+        }
+        return "";
+    }
+
+    @Override
+    public @Nls @NotNull String getNodeText(@NotNull final PsiElement element, final boolean useFullName) {
+        if (element instanceof PsiNamedElement) {
+            final String name = ((PsiNamedElement) element).getName();
+            if (name != null) {
+                return name;
+            }
+        }
+        return "";
+    }
+}
