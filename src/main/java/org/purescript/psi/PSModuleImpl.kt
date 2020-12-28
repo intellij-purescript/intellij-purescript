f7bcd901a6c82493a3adf2d3f3f138e86fe0aa93
Samuel Ytterbrink <neppord@gmail.com>
2020-12-28 11:23:11 +0100

    Format file

--- src/main/java/org/purescript/psi/PSModuleImpl.kt
+++ src/main/java/org/purescript/psi/PSModuleImpl.kt
@@ -3,10 +3,12 @@ package org.purescript.psi
 import com.intellij.lang.ASTNode
 import com.intellij.psi.util.PsiTreeUtil
 
-class PSModuleImpl(node: ASTNode) : PSPsiElement(node){
+class PSModuleImpl(node: ASTNode) : PSPsiElement(node) {
+
     val topLevelValueDeclarations: Map<String, PSValueDeclarationImpl>
-        get() = PsiTreeUtil.findChildrenOfType(this, PSValueDeclarationImpl::class.java)
+        get() = PsiTreeUtil
+            .findChildrenOfType(this, PSValueDeclarationImpl::class.java)
             .asSequence()
-            .map {Pair(it.name, it)}
+            .map { Pair(it.name, it) }
             .toMap()
 }
