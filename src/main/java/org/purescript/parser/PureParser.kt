47dd813c9597653e76a2ac79a91c05e001402eef
Samuel Ytterbrink <neppord@gmail.com>
2021-01-12 21:22:49 +0100

    Use or instead of choice when there is only 2 choices

--- src/main/java/org/purescript/parser/PureParser.kt
+++ src/main/java/org/purescript/parser/PureParser.kt
@@ -192,28 +192,23 @@ class PureParser : PsiParser, PSTokens, PSElements {
         private val parseRowEnding =
             optional(
                 indented(lexeme(PIPE))
-                .then(indented(choice(
-                        attempt(parseTypeWildcard),
-                        attempt(
-                            optional(
-                                lexeme(
-                                    manyOrEmpty(properName).`as`(TypeConstructor)
-                                )
+                .then(indented(
+                    attempt(parseTypeWildcard)
+                    .or(attempt(
+                        optional(
+                            lexeme(
+                                manyOrEmpty(properName).`as`(TypeConstructor)
                             )
-                            .then(
-                                    optional(
-                                        lexeme(idents).`as`(PSElements.GenericIdentifier)
-                                    )
-                                )
-                                .then(optional(indented(lexeme(
-                                    lname.or(stringLiteral)
-                                ))))
-                                .then(
-                                    optional(indented(dcolon))
-                                    .then(optional(type))
-                                ).`as`(PSElements.TypeVar)
                         )
-                )))
+                        .then(optional(
+                            lexeme(idents).`as`(PSElements.GenericIdentifier)
+                        )).then(optional(indented(lexeme(
+                            lname.or(stringLiteral)
+                        )))).then(
+                            optional(indented(dcolon)).then(optional(type))
+                        ).`as`(PSElements.TypeVar)
+                    ))
+                ))
             )
         private val parseRow: Parsec =
             commaSep(parseNameAndType(type))
