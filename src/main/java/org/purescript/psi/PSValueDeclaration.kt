36eaad90e85f69d2ee26de8188fcba434c937205
Samuel Ytterbrink <neppord@gmail.com>
2021-01-23 18:16:15 +0100

    Add presentation for value declaration

--- src/main/java/org/purescript/psi/PSValueDeclaration.kt
+++ src/main/java/org/purescript/psi/PSValueDeclaration.kt
@@ -1,15 +1,35 @@
 package org.purescript.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.psi.util.collectDescendantsOfType
+import javax.swing.Icon
 
 class PSValueDeclaration(node: ASTNode) : PSPsiElement(node),
     PsiNameIdentifierOwner {
     override fun getName(): String {
         return findChildByClass(PSIdentifierImpl::class.java)!!
             .name
+    }
+
+    override fun getPresentation(): ItemPresentation {
+        val name = this.name
+        val fileName = this.containingFile.name
+        return object : ItemPresentation {
+            override fun getPresentableText(): String {
+                return name
+            }
+
+            override fun getLocationString(): String {
+                return fileName
+            }
+
+            override fun getIcon(unused: Boolean): Icon? {
+                return null
+            }
+        }
     }
 
     override fun setName(name: String): PsiElement? {
