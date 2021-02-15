d4c83d04736765513754d163e261e336fa7a1847
Samuel Ytterbrink <neppord@gmail.com>
2021-02-15 21:22:17 +0100

    Fix rendering of md codeblocks in documentation

--- src/main/java/org/purescript/features/PSDocumentationProvider.kt
+++ src/main/java/org/purescript/features/PSDocumentationProvider.kt
@@ -3,6 +3,7 @@ package org.purescript.features
 import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.lang.documentation.DocumentationMarkup.*
 import com.intellij.psi.PsiElement
+import com.intellij.util.text.MarkdownUtil.replaceCodeBlock
 import com.petebevin.markdown.MarkdownProcessor
 import org.purescript.psi.PSModule
 import org.purescript.psi.PSValueDeclaration
@@ -31,10 +32,12 @@ class PSDocumentationProvider : AbstractDocumentationProvider() {
 
     fun docCommentsToDocstring(commentText: List<String>): String {
         val processor = MarkdownProcessor()
-        val markdown = commentText
+        val lines = commentText
             .map { it.trim() }
-            .map { it.removePrefix("-- |") }
-            .joinToString("\n") { it.trim() }
+            .map { it.removePrefix("-- |").trim() }
+            .toMutableList()
+        replaceCodeBlock(lines)
+        val markdown = lines.joinToString("\n") { it.trim() }
         return processor.markdown(markdown).trim()
     }
 
