49fae75998968c68792a94ee1c198864d2db9bc9
Samuel Ytterbrink <neppord@gmail.com>
2021-02-15 20:38:49 +0100

    Add null checks
    This needs some testing, but when trying out the feature i got null pointers and this is better then crashing at least.

--- src/main/java/org/purescript/psi/PSPositionedDeclarationRefImpl.kt
+++ src/main/java/org/purescript/psi/PSPositionedDeclarationRefImpl.kt
@@ -3,5 +3,9 @@ package org.purescript.psi
 import com.intellij.lang.ASTNode
 
 class PSPositionedDeclarationRefImpl(node: ASTNode) : PSPsiElement(node) {
-    val isModuleExport: Boolean get() = firstChild.firstChild.textMatches("module")
+    val isModuleExport: Boolean get() =
+        firstChild
+            ?.firstChild
+            ?.textMatches("module")
+            ?: false
 }
