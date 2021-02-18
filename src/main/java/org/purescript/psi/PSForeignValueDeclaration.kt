3966e0b70b342d019ef70a141e8def465fa219d9
Samuel Ytterbrink <neppord@gmail.com>
2021-02-18 19:37:21 +0100

    Give foreignValueDeclaration a name
    Co-authored-by: Simon Olander Sahlén <simon.olander@gmail.com>

--- src/main/java/org/purescript/psi/PSForeignValueDeclaration.kt
+++ src/main/java/org/purescript/psi/PSForeignValueDeclaration.kt
@@ -1,5 +1,20 @@
 package org.purescript.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNameIdentifierOwner
 
-class PSForeignValueDeclaration(node: ASTNode) : PSPsiElement(node)
+class PSForeignValueDeclaration(node: ASTNode) : PSPsiElement(node),
+    PsiNameIdentifierOwner {
+    override fun setName(name: String): PsiElement? {
+        return null
+    }
+
+    override fun getNameIdentifier(): PSIdentifierImpl? {
+        return findChildByClass(PSIdentifierImpl::class.java)
+    }
+
+    override fun getName(): String? {
+        return nameIdentifier?.name
+    }
+}
