db824a39fbe56444333593ef547d132c7f2323dc
Samuel Ytterbrink <neppord@gmail.com>
2021-02-20 20:41:40 +0100

    Implement documentation for foreign values

--- src/main/java/org/purescript/psi/PSForeignValueDeclaration.kt
+++ src/main/java/org/purescript/psi/PSForeignValueDeclaration.kt
@@ -1,11 +1,19 @@
 package org.purescript.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.elementType
+import org.purescript.features.DocCommentOwner
+import org.purescript.parser.PSTokens
 
-class PSForeignValueDeclaration(node: ASTNode) : PSPsiElement(node),
-    PsiNameIdentifierOwner {
+class PSForeignValueDeclaration(node: ASTNode) :
+    PSPsiElement(node),
+    PsiNameIdentifierOwner,
+    DocCommentOwner
+{
     override fun setName(name: String): PsiElement? {
         return null
     }
@@ -17,4 +25,14 @@ class PSForeignValueDeclaration(node: ASTNode) : PSPsiElement(node),
     override fun getName(): String? {
         return nameIdentifier?.name
     }
+
+    override val docComments: List<PsiComment>
+        get() =
+            generateSequence(prevSibling) { it.prevSibling }
+                .dropWhile { it !is PsiComment && it !is DocCommentOwner }
+                .takeWhile { it is PsiComment || it is PsiWhiteSpace }
+                .filter { it.elementType == PSTokens.DOC_COMMENT}
+                .filterIsInstance(PsiComment::class.java)
+                .toList()
+                .reversed()
 }
