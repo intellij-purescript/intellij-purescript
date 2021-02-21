182af8f378ddcdf168cce51f46afc8077ef6e4dd
Samuel Ytterbrink <neppord@gmail.com>
2021-02-21 11:57:24 +0100

    Simplify local value reference
    And remove helper members

--- src/main/java/org/purescript/psi/PSImportDeclarationImpl.kt
+++ src/main/java/org/purescript/psi/PSImportDeclarationImpl.kt
@@ -13,21 +13,23 @@ class PSImportDeclarationImpl(node: ASTNode) : PSPsiElement(node) {
      *
      * `import Lib (namedImports)`
      * */
-    val namedImports: List<String> get() =
-        findChildrenByClass(PSPositionedDeclarationRefImpl::class.java)
-            .asSequence()
-            .map { it.text.trim() }
-            .toList()
+    val namedImports: List<String>
+        get() =
+            findChildrenByClass(PSPositionedDeclarationRefImpl::class.java)
+                .asSequence()
+                .map { it.text.trim() }
+                .toList()
 
     /** is the import statement a hiding
      *
      * `import Lib hiding (x)`
      * */
-    val isHiding: Boolean get() =
-        SyntaxTraverser.psiTraverser(this)
-            .expand { it !is PSPositionedDeclarationRefImpl }
-            .filterIsInstance(LeafPsiElement::class.java)
-            .any { it.text.trim() == "hiding" }
+    val isHiding: Boolean
+        get() =
+            SyntaxTraverser.psiTraverser(this)
+                .expand { it !is PSPositionedDeclarationRefImpl }
+                .filterIsInstance(LeafPsiElement::class.java)
+                .any { it.text.trim() == "hiding" }
 
 
     val importName get() = findChildByClass(PSProperName::class.java)
@@ -38,18 +40,25 @@ class PSImportDeclarationImpl(node: ASTNode) : PSPsiElement(node) {
 
     private val importedModule get(): PSModule? = ModuleReference(this).resolve()
 
-    val importedValues get(): Sequence<PSValueDeclaration> =
-        when {
-            isHiding -> {
-                importedModule?.exportedValuesExcluding(namedImports.toSet())
-            }
-            namedImports.isNotEmpty() -> {
-                importedModule?.exportedValuesMatching(namedImports.toSet())
-            }
-            else -> {
-                importedModule?.exportedValueDeclarations
-            }
-        }?: sequenceOf()
+    val importedValues
+        get(): Sequence<PSValueDeclaration> =
+            when {
+                isHiding -> {
+                    importedModule
+                        ?.exportedValueDeclarations
+                        ?.filter { it.name !in namedImports.toSet() }
+                        ?.asSequence()
+                }
+                namedImports.isNotEmpty() -> {
+                    importedModule
+                        ?.exportedValueDeclarations
+                        ?.filter { it.name in namedImports.toSet() }
+                        ?.asSequence()
+                }
+                else -> {
+                    importedModule?.exportedValueDeclarations?.asSequence()
+                }
+            } ?: sequenceOf()
 
 
     fun isNotHidingName(name: String): Boolean {
