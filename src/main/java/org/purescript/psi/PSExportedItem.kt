4910966b1b3f0fdf9aa0ea9f73298be56556c25b
Simon Olander <simon.olander@gmail.com>
2021-02-25 18:52:54 +0100

    Add resolution, completion, and inspections for exported modules

--- src/main/java/org/purescript/psi/PSExportedItem.kt
+++ src/main/java/org/purescript/psi/PSExportedItem.kt
@@ -2,6 +2,7 @@ package org.purescript.psi
 
 import com.intellij.lang.ASTNode
 import org.purescript.file.PSFile
+import org.purescript.psi.`var`.ExportedModuleReference
 import org.purescript.psi.`var`.ExportedValueReference
 
 sealed class PSExportedItem(node: ASTNode) : PSPsiElement(node) {
@@ -14,9 +15,18 @@ class PSExportedData(node: ASTNode) : PSExportedItem(node) {
 
 class PSExportedClass(node: ASTNode) : PSExportedItem(node)
 class PSExportedKind(node: ASTNode) : PSExportedItem(node)
-class PSExportedModule(node: ASTNode) : PSExportedItem(node)
 class PSExportedOperator(node: ASTNode) : PSExportedItem(node)
 class PSExportedType(node: ASTNode) : PSExportedItem(node)
+
+class PSExportedModule(node: ASTNode) : PSExportedItem(node) {
+    val qualifiedIdentifier = findNotNullChildByClass(PSProperName::class.java)
+
+    override fun getName(): String = qualifiedIdentifier.name
+
+    override fun getReference(): ExportedModuleReference {
+        return ExportedModuleReference(this)
+    }
+}
 
 class PSExportedValue(node: ASTNode) : PSExportedItem(node) {
 
