9cc26614743f326e144318286e57e9bac924f691
Samuel Ytterbrink <neppord@gmail.com>
2021-03-13 15:32:56 +0100

    Static import method to reduce noise

--- src/main/java/org/purescript/highlighting/PSSyntaxHighlightAnnotator.kt
+++ src/main/java/org/purescript/highlighting/PSSyntaxHighlightAnnotator.kt
@@ -3,28 +3,28 @@ package org.purescript.highlighting
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.psi.PsiElement
 import org.purescript.parser.PSElements
 
 class PSSyntaxHighlightAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (PlatformPatterns.psiElement(PSElements.ValueRef).accepts(element)) {
+        if (psiElement(PSElements.ValueRef).accepts(element)) {
             val text = element.text
             holder.newAnnotation(HighlightSeverity.INFORMATION, text)
                 .textAttributes(PSSyntaxHighlighter.IMPORT_REF)
                 .create()
-        } else if (PlatformPatterns.psiElement(PSElements.TypeAnnotationName)
+        } else if (psiElement(PSElements.TypeAnnotationName)
                 .accepts(element)
         ) {
             holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
                 .textAttributes(PSSyntaxHighlighter.TYPE_ANNOTATION_NAME)
                 .create()
-        } else if (PlatformPatterns.psiElement(PSElements.PositionedDeclarationRef)
+        } else if (psiElement(PSElements.PositionedDeclarationRef)
                 .accepts(element)
-            || PlatformPatterns.psiElement(PSElements.TypeConstructor)
+            || psiElement(PSElements.TypeConstructor)
                 .accepts(element)
-            || PlatformPatterns.psiElement(PSElements.pClassName)
+            || psiElement(PSElements.pClassName)
                 .accepts(element)
         ) {
 //                || psiElement(PSElements.pModuleName).accepts(element))) {
@@ -32,17 +32,17 @@ class PSSyntaxHighlightAnnotator : Annotator {
                 .newAnnotation(HighlightSeverity.INFORMATION, element.text)
                 .textAttributes(PSSyntaxHighlighter.TYPE_NAME)
                 .create()
-        } else if (PlatformPatterns.psiElement(PSElements.GenericIdentifier)
+        } else if (psiElement(PSElements.GenericIdentifier)
                 .accepts(element)
-            || PlatformPatterns.psiElement(PSElements.Constructor)
+            || psiElement(PSElements.Constructor)
                 .accepts(element)
-            || PlatformPatterns.psiElement(PSElements.qualifiedModuleName)
+            || psiElement(PSElements.qualifiedModuleName)
                 .accepts(element)
         ) {
             holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
                 .textAttributes(PSSyntaxHighlighter.TYPE_VARIABLE)
                 .create()
-        } else if (PlatformPatterns.psiElement(PSElements.LocalIdentifier)
+        } else if (psiElement(PSElements.LocalIdentifier)
                 .accepts(element)
         ) {
             holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
