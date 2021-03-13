1766add8c1b654ac3d32e67ce0ef6704adc31089
Samuel Ytterbrink <neppord@gmail.com>
2021-03-13 15:52:38 +0100

    Use silent annotations

--- src/main/java/org/purescript/highlighting/PSSyntaxHighlightAnnotator.kt
+++ src/main/java/org/purescript/highlighting/PSSyntaxHighlightAnnotator.kt
@@ -10,29 +10,27 @@ class PSSyntaxHighlightAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         when (element.node.elementType) {
             PSElements.ValueRef -> {
-                val text = element.text
-                holder.newAnnotation(HighlightSeverity.INFORMATION, text)
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                     .textAttributes(PSSyntaxHighlighter.IMPORT_REF)
                     .create()
             }
             PSElements.TypeAnnotationName -> {
-                holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                     .textAttributes(PSSyntaxHighlighter.TYPE_ANNOTATION_NAME)
                     .create()
             }
             PSElements.PositionedDeclarationRef, PSElements.TypeConstructor, PSElements.pClassName -> {
-                holder
-                    .newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                     .textAttributes(PSSyntaxHighlighter.TYPE_NAME)
                     .create()
             }
             PSElements.GenericIdentifier, PSElements.Constructor, PSElements.qualifiedModuleName -> {
-                holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                     .textAttributes(PSSyntaxHighlighter.TYPE_VARIABLE)
                     .create()
             }
             PSElements.LocalIdentifier -> {
-                holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
                     .textAttributes(PSSyntaxHighlighter.NUMBER)
                     .create()
             }
