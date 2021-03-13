26906ab14507e7443534e17fcd62208e85453bbf
Samuel Ytterbrink <neppord@gmail.com>
2021-03-13 15:43:32 +0100

    Replace else if with when

--- src/main/java/org/purescript/highlighting/PSSyntaxHighlightAnnotator.kt
+++ src/main/java/org/purescript/highlighting/PSSyntaxHighlightAnnotator.kt
@@ -8,34 +8,34 @@ import org.purescript.parser.PSElements
 
 class PSSyntaxHighlightAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element.node.elementType == PSElements.ValueRef) {
-            val text = element.text
-            holder.newAnnotation(HighlightSeverity.INFORMATION, text)
-                .textAttributes(PSSyntaxHighlighter.IMPORT_REF)
-                .create()
-        } else if (element.node.elementType == PSElements.TypeAnnotationName) {
-            holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
-                .textAttributes(PSSyntaxHighlighter.TYPE_ANNOTATION_NAME)
-                .create()
-        } else if (element.node.elementType == PSElements.PositionedDeclarationRef
-            || element.node.elementType == PSElements.TypeConstructor
-            || element.node.elementType == PSElements.pClassName) {
-            holder
-                .newAnnotation(HighlightSeverity.INFORMATION, element.text)
-                .textAttributes(PSSyntaxHighlighter.TYPE_NAME)
-                .create()
-        } else if (element.node.elementType == PSElements.GenericIdentifier
-            || element.node.elementType == PSElements.Constructor
-            || element.node.elementType == PSElements.qualifiedModuleName
-        ) {
-            holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
-                .textAttributes(PSSyntaxHighlighter.TYPE_VARIABLE)
-                .create()
-        } else if (element.node.elementType == PSElements.LocalIdentifier
-        ) {
-            holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
-                .textAttributes(PSSyntaxHighlighter.NUMBER)
-                .create()
+        when (element.node.elementType) {
+            PSElements.ValueRef -> {
+                val text = element.text
+                holder.newAnnotation(HighlightSeverity.INFORMATION, text)
+                    .textAttributes(PSSyntaxHighlighter.IMPORT_REF)
+                    .create()
+            }
+            PSElements.TypeAnnotationName -> {
+                holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                    .textAttributes(PSSyntaxHighlighter.TYPE_ANNOTATION_NAME)
+                    .create()
+            }
+            PSElements.PositionedDeclarationRef, PSElements.TypeConstructor, PSElements.pClassName -> {
+                holder
+                    .newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                    .textAttributes(PSSyntaxHighlighter.TYPE_NAME)
+                    .create()
+            }
+            PSElements.GenericIdentifier, PSElements.Constructor, PSElements.qualifiedModuleName -> {
+                holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                    .textAttributes(PSSyntaxHighlighter.TYPE_VARIABLE)
+                    .create()
+            }
+            PSElements.LocalIdentifier -> {
+                holder.newAnnotation(HighlightSeverity.INFORMATION, element.text)
+                    .textAttributes(PSSyntaxHighlighter.NUMBER)
+                    .create()
+            }
         }
     }
 }
