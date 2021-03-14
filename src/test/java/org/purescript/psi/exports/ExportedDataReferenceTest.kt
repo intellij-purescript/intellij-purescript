b2215c4589af2791db8502856d0cb101f6d98d8d
Simon Olander <simon.olander@gmail.com>
2021-03-14 18:12:54 +0100

    Fix find usage for newtype declarations

--- src/test/java/org/purescript/psi/exports/ExportedDataReferenceTest.kt
+++ src/test/java/org/purescript/psi/exports/ExportedDataReferenceTest.kt
@@ -57,4 +57,56 @@ class ExportedDataReferenceTest : BasePlatformTestCase() {
 
         TestCase.assertEquals(exportedData, usageInfo.element)
     }
+
+    fun `test resolves to newtype declaration`() {
+        val module = myFixture.configureByText(
+            "Foo.purs",
+            """
+                module Foo (Bar) where
+                newtype Bar = Qux Int
+            """.trimIndent()
+        ).getModule()
+        val exportedData = module.exportList!!.exportedItems.single() as PSExportedData
+        val newtypeDeclaration = module.newTypeDeclarations.single()
+
+        TestCase.assertEquals(newtypeDeclaration, exportedData.reference.resolve())
+    }
+
+    fun `test resolve fails when no newtype declaration exists`() {
+        val module = myFixture.configureByText(
+            "Foo.purs",
+            """
+                module Foo (Bar) where
+            """.trimIndent()
+        ).getModule()
+        val exportedData = module.exportList!!.exportedItems.single() as PSExportedData
+
+        TestCase.assertNull(exportedData.reference.resolve())
+    }
+
+    fun `test completes newtype declarations`() {
+        myFixture.configureByText(
+            "Foo.purs",
+            """
+                module Foo (B<caret>) where
+                newtype Bar = Qux Int
+                newtype Baz = Qux Int
+            """.trimIndent()
+        )
+        myFixture.testCompletionVariants("Foo.purs", "Bar", "Baz")
+    }
+
+    fun `test finds usage of newtype declaration`() {
+        val module = myFixture.configureByText(
+            "Foo.purs",
+            """
+                module Foo (Bar) where
+                newtype <caret>Bar = Qux Int
+            """.trimIndent()
+        ).getModule()
+        val exportedData = module.exportList!!.exportedItems.single() as PSExportedData
+        val usageInfo = myFixture.testFindUsages("Foo.purs").single()
+
+        TestCase.assertEquals(exportedData, usageInfo.element)
+    }
 }
