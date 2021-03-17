0e6f30eee01ddb33d013e124fe9e217c13744fc0
Samuel Ytterbrink <neppord@gmail.com>
2021-03-17 20:16:38 +0100

    Remove extra attempt

--- src/main/java/org/purescript/parser/PureParsecParser.kt
+++ src/main/java/org/purescript/parser/PureParsecParser.kt
@@ -829,7 +829,7 @@ class PureParsecParser {
             .or(many1(idents.`as`(ProperName)))
         val expr5 = choice(
             attempt(indented(braces(commaSep1(indented(parsePropertyUpdate))))),
-            exprAtom + manyOrEmpty(attempt(token(DOT) + label).`as`(Accessor)),
+            exprAtom + manyOrEmpty((token(DOT) + label).`as`(Accessor)),
             attempt(tick + exprBacktick + tick),
             abs,
             (case + commaSep1(expr) + of + indentedList(caseBranch)).`as`(Case),
