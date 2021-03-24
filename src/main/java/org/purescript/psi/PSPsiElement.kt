b3c5c3d8cc1280962dab1005d0cf5da5de10a495
Simon Olander <simon.olander@gmail.com>
2021-03-24 19:07:15 +0100

    Create module property for PSPsiElement

--- src/main/java/org/purescript/psi/PSPsiElement.kt
+++ src/main/java/org/purescript/psi/PSPsiElement.kt
@@ -2,5 +2,12 @@ package org.purescript.psi
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
+import org.purescript.file.PSFile
 
-abstract class PSPsiElement(node: ASTNode) : ASTWrapperPsiElement(node)
+abstract class PSPsiElement(node: ASTNode) : ASTWrapperPsiElement(node) {
+
+    /**
+     * @return the [PSModule] containing this element
+     */
+    val module: PSModule get() = (containingFile as PSFile).module
+}
