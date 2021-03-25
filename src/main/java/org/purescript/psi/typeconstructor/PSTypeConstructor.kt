8b5a2c17c5780a5497a5b2900e1b7fafdb4ad7c9
Samuel Ytterbrink <neppord@gmail.com>
2021-03-25 19:31:19 +0100

    Move type constructor psi element to own package
    Co-authored-by: Simon Olander Sahlén <simon.olander@gmail.com>
    Co-authored-by: Olof Bjarnason <olof.bjarnason@gmail.com>
    Co-authored-by: Serge S. Gulin <gulin.serge@gmail.com>

--- src/main/java/org/purescript/psi/typeconstructor/PSTypeConstructor.kt
+++ src/main/java/org/purescript/psi/typeconstructor/PSTypeConstructor.kt
@@ -1,5 +1,6 @@
-package org.purescript.psi
+package org.purescript.psi.typeconstructor
 
 import com.intellij.lang.ASTNode
+import org.purescript.psi.PSPsiElement
 
 class PSTypeConstructor(node: ASTNode) : PSPsiElement(node)
