709eb1e44020a19d819cd05a9e52bb341cd6a2c0
Simon Olander <simon.olander@gmail.com>
2021-03-26 20:45:17 +0100

    Add reference support between type constructors and data declarations and newtype declarations

--- src/main/java/org/purescript/psi/typeconstructor/TypeConstructorReference.kt
+++ src/main/java/org/purescript/psi/typeconstructor/TypeConstructorReference.kt
@@ -2,6 +2,7 @@ package org.purescript.psi.typeconstructor
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
+import org.purescript.psi.PSPsiElement
 
 class TypeConstructorReference(typeConstructor: PSTypeConstructor) :
     PsiReferenceBase<PSTypeConstructor>(
@@ -9,7 +10,20 @@ class TypeConstructorReference(typeConstructor: PSTypeConstructor) :
         typeConstructor.textRangeInParent,
         false
     ) {
-    override fun resolve(): PsiElement? {
-        return myElement.module.dataDeclarations.firstOrNull {it.name == myElement.name}
-    }
+
+    override fun getVariants(): Array<Any> =
+        candidates.toTypedArray()
+
+    override fun resolve(): PsiElement? =
+        candidates.firstOrNull { it.name == myElement.name }
+
+    /*
+     * TODO [simonolander]
+     *  Add support for type declarations
+     */
+    private val candidates: List<PSPsiElement>
+        get() = myElement.module.run {
+            dataDeclarations.toList() + newTypeDeclarations.toList() +
+                importDeclarations.flatMap { it.importedDataDeclarations + it.importedNewTypeDeclarations }
+        }
 }
