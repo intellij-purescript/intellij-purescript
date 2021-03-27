f6e1f2c1598f66027dc3a500b84963f834c6e8ca
Simon Olander <simon.olander@gmail.com>
2021-03-27 19:47:23 +0100

    Add tests for type synonyms

---
+++ src/test/java/org/purescript/psi/typesynonym/PSTypeSynonymDeclarationTest.kt
@@ -0,0 +1,19 @@
+package org.purescript.psi.typesynonym
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.purescript.getTypeSynonymDeclaration
+
+class PSTypeSynonymDeclarationTest : BasePlatformTestCase() {
+
+    fun `test get name`() {
+        val typeSynonymDeclaration = myFixture.configureByText(
+            "Foo.purs",
+            """
+                module Foo where
+                type Bar = Int
+            """.trimIndent()
+        ).getTypeSynonymDeclaration()
+
+        assertEquals("Bar", typeSynonymDeclaration.name)
+    }
+}
