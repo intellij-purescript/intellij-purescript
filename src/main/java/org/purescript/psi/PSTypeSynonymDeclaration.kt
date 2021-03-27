36297796c38b1c464572eedb0de8060dfe58d6b2
Simon Olander <simon.olander@gmail.com>
2021-03-27 19:40:14 +0100

    Make PSTypeSynonymDeclaration implement PsiNameIdentifierOwner

--- src/main/java/org/purescript/psi/PSTypeSynonymDeclaration.kt
+++ src/main/java/org/purescript/psi/PSTypeSynonymDeclaration.kt
@@ -1,6 +1,33 @@
 package org.purescript.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNameIdentifierOwner
 
+/**
+ * A type synonym declaration, e.g.
+ * ```
+ * type GlobalEvents r = ( onContextMenu :: Event | r )
+ * ```
+ */
+class PSTypeSynonymDeclaration(node: ASTNode) :
+    PSPsiElement(node),
+    PsiNameIdentifierOwner {
 
-class PSTypeSynonymDeclaration(node: ASTNode) : PSPsiElement(node)
+    /**
+     * @return the [PSProperName] identifying this declaration
+     */
+    private val identifier: PSProperName
+        get() = findNotNullChildByClass(PSProperName::class.java)
+
+    override fun setName(name: String): PsiElement? {
+        return null
+    }
+
+    override fun getNameIdentifier(): PsiElement = identifier
+
+    override fun getName(): String = identifier.name
+
+    override fun getTextOffset(): Int = identifier.textOffset
+
+}
