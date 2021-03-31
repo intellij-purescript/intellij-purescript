3c0537307dacaca00b128d04ab4710a7330b3051
Simon Olander <simon.olander@gmail.com>
2021-03-31 11:06:56 +0200

    Make ExportedModuleReference#handleElementRename use PSPsiFactory

--- src/main/java/org/purescript/psi/exports/ExportedModuleReference.kt
+++ src/main/java/org/purescript/psi/exports/ExportedModuleReference.kt
@@ -1,10 +1,8 @@
 package org.purescript.psi.exports
 
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiReferenceBase
-import org.purescript.PSLanguage
-import org.purescript.file.PSFile
+import org.purescript.psi.PSPsiFactory
 import org.purescript.psi.imports.PSImportDeclarationImpl
 
 class ExportedModuleReference(exportedModule: PSExportedModule) : PsiReferenceBase<PSExportedModule>(
@@ -21,15 +19,8 @@ class ExportedModuleReference(exportedModule: PSExportedModule) : PsiReferenceBa
         candidates.firstOrNull { it.name == myElement.name }
             ?.run { importAlias ?: importedModule }
 
-    override fun handleElementRename(newElementName: String): PsiElement? {
-        val psiFileFactory = PsiFileFactory.getInstance(element.project)
-        val file = psiFileFactory.createFileFromText(
-            PSLanguage.INSTANCE,
-            """
-                module $newElementName where
-            """.trimIndent()
-        ) as PSFile
-        val newProperName = file.module?.nameIdentifier
+    override fun handleElementRename(name: String): PsiElement? {
+        val newProperName = PSPsiFactory(element.project).createQualifiedProperName(name)
             ?: return null
         element.properName.replace(newProperName)
         return element
