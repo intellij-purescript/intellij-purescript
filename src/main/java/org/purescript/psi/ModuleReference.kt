b2dc71db949bafef2f45692f6a18083f1e823ae4
Samuel Ytterbrink <neppord@gmail.com>
2021-03-31 10:41:28 +0200

    Use psi file gist to cache module names for file

--- src/main/java/org/purescript/psi/ModuleReference.kt
+++ src/main/java/org/purescript/psi/ModuleReference.kt
@@ -4,10 +4,23 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiReferenceBase
 import com.intellij.psi.search.FilenameIndex
-import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.util.gist.GistManager
+import com.intellij.util.io.EnumeratorStringDescriptor
 import org.purescript.file.PSFile
 import org.purescript.file.PSFileType
 import org.purescript.psi.imports.PSImportDeclarationImpl
+
+val gistManager = GistManager.getInstance()
+val newPsiFileGist = gistManager.newPsiFileGist(
+    "module name",
+    0,
+    EnumeratorStringDescriptor()
+) { file ->
+    when (file) {
+        is PSFile -> file.module?.name
+        else -> null
+    }
+}
 
 class ModuleReference(element: PSImportDeclarationImpl) : PsiReferenceBase<PSImportDeclarationImpl>(
     element,
@@ -15,25 +28,28 @@ class ModuleReference(element: PSImportDeclarationImpl) : PsiReferenceBase<PSImp
     false
 ) {
     override fun getVariants(): Array<PSModule> {
-        val project = element.project
-        val psiManager = PsiManager.getInstance(project)
-        return FilenameIndex.getAllFilesByExt(project, PSFileType.DEFAULT_EXTENSION)
-            .mapNotNull { psiManager.findFile(it) }
-            .filterIsInstance<PSFile>()
+        return candidateFiles
             .mapNotNull { it.module }
             .toTypedArray()
     }
+
+    private val candidateFiles: List<PSFile>
+        get() {
+            val project = element.project
+            val psiManager = PsiManager.getInstance(project)
+            val filterIsInstance = FilenameIndex
+                .getAllFilesByExt(project, PSFileType.DEFAULT_EXTENSION)
+                .mapNotNull { psiManager.findFile(it) }
+                .filterIsInstance<PSFile>()
+            return filterIsInstance
+        }
 
     override fun resolve(): PSModule? {
         val moduleName = element.importName?.name
             ?: return null
         val fileName = moduleName.split(".").last() + ".purs"
-        return FilenameIndex.getFilesByName(
-            myElement.project,
-            fileName,
-            GlobalSearchScope.allScope(myElement.project)
-        ).filterIsInstance<PSFile>()
-            .mapNotNull { it.module }
-            .firstOrNull { it.name == moduleName }
+        val file = candidateFiles
+            .firstOrNull { newPsiFileGist.getFileData(it) == moduleName }
+        return file?.module
     }
 }
