2f8c152f477d3b6ea180fa6bbab6de7835b4e0ed
Simon Olander <simon.olander@gmail.com>
2021-03-31 16:46:40 +0200

    Add PSRefactoringSupportProvider to enable in-place renaming

---
+++ src/main/java/org/purescript/ide/refactoring/PSRefactoringSupportProvider.kt
@@ -0,0 +1,11 @@
+package org.purescript.ide.refactoring
+
+import com.intellij.lang.refactoring.RefactoringSupportProvider
+import com.intellij.psi.PsiElement
+import org.purescript.psi.PSModule
+
+class PSRefactoringSupportProvider : RefactoringSupportProvider() {
+    override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
+        return element is PSModule
+    }
+}
