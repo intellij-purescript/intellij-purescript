21d52c6ec93614f22ce36c45b8c10cf3bc0fa458
Samuel Ytterbrink <neppord@gmail.com>
2021-04-03 20:13:19 +0200

    Quick fix so that we can release using EAP

--- src/main/java/org/purescript/psi/PSPsiFactory.kt
+++ src/main/java/org/purescript/psi/PSPsiFactory.kt
@@ -4,9 +4,46 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiParserFacade
-import com.intellij.psi.util.findDescendantOfType
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import org.purescript.PSLanguage
 import org.purescript.psi.imports.PSImportDeclarationImpl
+
+
+/**
+ * This should be [com.intellij.psi.util.findDescendantOfType]
+ * but is currently missing from the EAP build
+ *
+ * Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ */
+inline fun <reified T : PsiElement> PsiElement.findDescendantOfType(noinline predicate: (T) -> Boolean = { true }): T? {
+    return findDescendantOfType({ true }, predicate)
+}
+/**
+ * This should be [com.intellij.psi.util.findDescendantOfType]
+ * but is currently missing from the EAP build
+ *
+ * Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ */
+inline fun <reified T : PsiElement> PsiElement.findDescendantOfType(
+    crossinline canGoInside: (PsiElement) -> Boolean,
+    noinline predicate: (T) -> Boolean = { true }
+): T? {
+    var result: T? = null
+    this.accept(object : PsiRecursiveElementWalkingVisitor() {
+        override fun visitElement(element: PsiElement) {
+            if (element is T && predicate(element)) {
+                result = element
+                stopWalking()
+                return
+            }
+
+            if (canGoInside(element)) {
+                super.visitElement(element)
+            }
+        }
+    })
+    return result
+}
 
 class PSPsiFactory(private val project: Project) {
 
