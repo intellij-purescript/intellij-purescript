af93e52cd32f3e0669caf2778a4d9d4a362a910c
typist <typist@code-op.se>
2021-04-05 17:33:47 +0000

    Lex some example code with the help of purescript-cst-parser

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,32 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Class.Console (log)
+import PureScript.CST.Lexer
+import PureScript.CST.TokenStream
+import Debug (traceM)
+import Data.Maybe
+import Data.Array
+
+firstToken tokens = case step tokens of
+    TokenCons r _ next _ ->  r.value : firstToken next
+    _ -> []
+
+
+src =
+    """main = do
+      let t1 = test1
+      let t2 = test2
+      log "Done"
+    """
+
+typeSrc = """
+module Main where
+test1 :: forall r . S { foo :: String | r } Unit
+test1 = state $ \o -> o { foo = o.foo <> "!" }
+"""
 
 main :: Effect Unit
 main = do
-  log "🍝"
-  log "You should add some tests."
+  let tokens = lex typeSrc
+  log typeSrc
+  traceM $ firstToken tokens
