44aae7c1275ecfefb3155ebf21af741665f995ce
Simon Olander <simon.olander@gmail.com>
2021-04-06 18:57:15 +0200

    Add qualified variants of names

---
+++ src/main/java/org/purescript/psi/name/PSQualifiedProperName.kt
@@ -0,0 +1,13 @@
+package org.purescript.psi.name
+
+import com.intellij.lang.ASTNode
+import org.purescript.psi.PSPsiElement
+
+class PSQualifiedProperName(node: ASTNode) : PSPsiElement(node) {
+
+    val moduleName: PSModuleName?
+        get() = findChildByClass(PSModuleName::class.java)
+
+    val properName: PSProperName
+        get() = findNotNullChildByClass(PSProperName::class.java)
+}
