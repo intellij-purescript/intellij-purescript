b89d18d11d2e2e95bd211bd373817182207df6ac
Samuel Ytterbrink <neppord@gmail.com>
2021-04-14 19:40:23 +0200

    Use Constructor Reference for Constructor binder

--- src/main/java/org/purescript/psi/PSConstructorBinderImpl.kt
+++ src/main/java/org/purescript/psi/PSConstructorBinderImpl.kt
@@ -1,5 +1,18 @@
 package org.purescript.psi
 
 import com.intellij.lang.ASTNode
+import org.purescript.psi.expression.ConstructorReference
+import org.purescript.psi.name.PSQualifiedProperName
 
-class PSConstructorBinderImpl(node: ASTNode) : PSPsiElement(node)
+class PSConstructorBinderImpl(node: ASTNode) : PSPsiElement(node) {
+    /**
+     * @return the [PSQualifiedProperName] identifying this constructor
+     */
+    internal val qualifiedProperName: PSQualifiedProperName
+        get() = findNotNullChildByClass(PSQualifiedProperName::class.java)
+
+    override fun getName(): String = qualifiedProperName.name
+
+    override fun getReference(): ConstructorReference =
+        ConstructorReference(this, this.qualifiedProperName)
+}
