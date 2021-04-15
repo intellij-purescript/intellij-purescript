31994158685fbefcb22e04a4eebc1abf1f1d0390
Samuel Ytterbrink <neppord@gmail.com>
2021-04-15 16:35:01 +0200

    Parse class signatures

--- src/test/java/org/purescript/parser/ClassDeclarationTest.kt
+++ src/test/java/org/purescript/parser/ClassDeclarationTest.kt
@@ -2,4 +2,5 @@ package org.purescript.parser
 
 class ClassDeclarationTest: PSLanguageParserTestBase("class") {
     fun testWithConstraint() = doTest(true, true)
+    fun testClassSignature() = doTest(true, true)
 }
---
+++ test-data/class/ClassSignature.purs
@@ -0,0 +1,3 @@
+module ClassSignature where
+
+class MonoidRecord :: RL.RowList Type -> Row Type -> Row Type -> Constraint
