f40754469787bc8eef27bbc47894c2e7dd0cf0c1
Samuel Ytterbrink <neppord@gmail.com>
2021-04-20 20:54:02 +0200

    Add index over modules imported by file

---
+++ src/main/java/org/purescript/file/ImportedModuleIndex.kt
@@ -0,0 +1,65 @@
+package org.purescript.file
+
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.util.indexing.*
+import com.intellij.util.io.EnumeratorStringDescriptor
+import com.intellij.util.io.KeyDescriptor
+import org.jetbrains.annotations.NonNls
+
+class ImportedModuleIndex : ScalarIndexExtension<String>() {
+    override fun getName(): ID<String, Void?> {
+        return NAME
+    }
+
+    override fun getIndexer(): DataIndexer<String, Void?, FileContent> {
+        return DataIndexer<String, Void?, FileContent> {
+            when (val file = it.psiFile) {
+                is PSFile -> file.module
+                    ?.importDeclarations
+                    ?.mapNotNull { it.moduleName?.name}
+                    ?.map { it to null}
+                    ?.toMap()
+                    ?: emptyMap()
+                else -> emptyMap()
+            }
+        }
+    }
+
+    override fun getKeyDescriptor(): KeyDescriptor<String> =
+        EnumeratorStringDescriptor.INSTANCE
+
+    override fun getVersion(): Int = 0
+
+    override fun getInputFilter(): FileBasedIndex.InputFilter =
+        DefaultFileTypeSpecificInputFilter(PSFileType.INSTANCE)
+
+    override fun dependsOnFileContent(): Boolean = true
+
+    companion object {
+        @NonNls
+        val NAME =
+            ID.create<String, Void?>("org.purescript.file.ImportedModuleIndex")
+
+        fun filesImportingModule(
+            project: Project,
+            moduleName: String
+        ): List<PSFile> {
+            val fileBasedIndex = FileBasedIndex.getInstance()
+            val files =  ReadAction.compute<Collection<VirtualFile>, Throwable> {
+                fileBasedIndex.getContainingFiles(
+                    NAME,
+                    moduleName,
+                    GlobalSearchScope.allScope(project)
+                )
+            }
+            return files
+                .map { PsiManager.getInstance(project).findFile(it) }
+                .filterIsInstance(PSFile::class.java)
+        }
+    }
+
+}
