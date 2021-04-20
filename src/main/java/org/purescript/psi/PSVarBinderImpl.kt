c78fe693473f38a962a6e6228d1cd5d5f7ac1dcb
Samuel Ytterbrink <neppord@gmail.com>
2021-04-20 20:15:41 +0200

    Search for references to var binders only in current file

--- src/main/java/org/purescript/psi/PSVarBinderImpl.kt
+++ src/main/java/org/purescript/psi/PSVarBinderImpl.kt
@@ -3,6 +3,8 @@ package org.purescript.psi
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SearchScope
 import org.purescript.psi.name.PSIdentifier
 
 class PSVarBinderImpl(node: ASTNode) :
@@ -16,5 +18,9 @@ class PSVarBinderImpl(node: ASTNode) :
 
     override fun getNameIdentifier(): PSIdentifier {
         return findChildByClass(PSIdentifier::class.java)!!
+    }
+
+    override fun getUseScope(): SearchScope {
+        return LocalSearchScope(containingFile)
     }
 }
