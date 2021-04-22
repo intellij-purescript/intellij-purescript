5d1fee73aa3069d9015d406622f2ed5957281771
Samuel Ytterbrink <neppord@gmail.com>
2021-04-22 19:39:11 +0200

    Ad getter for name of signature
    Co-authored-by: Simon Olander Sahlén <simon.olander@gmail.com>

--- src/main/java/org/purescript/psi/PSSignature.kt
+++ src/main/java/org/purescript/psi/PSSignature.kt
@@ -1,6 +1,8 @@
 package org.purescript.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiReference
+import org.purescript.psi.name.PSIdentifier
 
 /**
  * `foo :: int` in
@@ -9,4 +11,15 @@ import com.intellij.lang.ASTNode
  * foo = 42
  * ```
  */
-class PSSignature(node: ASTNode) : PSPsiElement(node)
+class PSSignature(node: ASTNode) : PSPsiElement(node) {
+    val identifier get() =
+        findNotNullChildByClass(PSIdentifier::class.java)
+
+    override fun getName(): String {
+        return identifier.name
+    }
+
+    override fun getReference(): PsiReference? {
+        return super.getReference()
+    }
+}
