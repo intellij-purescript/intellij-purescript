4143a4a7a5207f3435956773dffad70ad453367a
Samuel Ytterbrink <neppord@gmail.com>
2021-04-29 19:59:44 +0200

    Partly implement operator finde usage

--- src/main/java/org/purescript/features/PSFindUsageProvider.kt
+++ src/main/java/org/purescript/features/PSFindUsageProvider.kt
@@ -1,13 +1,47 @@
 package org.purescript.features
 
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner
+import com.intellij.lang.cacheBuilder.WordsScanner
 import com.intellij.lang.findUsages.FindUsagesProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNamedElement
-import org.purescript.psi.*
+import com.intellij.psi.tree.TokenSet
+import org.purescript.lexer.PSLexer
+import org.purescript.parser.PSTokens.Companion.ARROW
+import org.purescript.parser.PSTokens.Companion.BACKSLASH
+import org.purescript.parser.PSTokens.Companion.CHAR
+import org.purescript.parser.PSTokens.Companion.COMMA
+import org.purescript.parser.PSTokens.Companion.DARROW
+import org.purescript.parser.PSTokens.Companion.DCOLON
+import org.purescript.parser.PSTokens.Companion.DDOT
+import org.purescript.parser.PSTokens.Companion.DOC_COMMENT
+import org.purescript.parser.PSTokens.Companion.DOT
+import org.purescript.parser.PSTokens.Companion.EQ
+import org.purescript.parser.PSTokens.Companion.FLOAT
+import org.purescript.parser.PSTokens.Companion.IDENT
+import org.purescript.parser.PSTokens.Companion.LARROW
+import org.purescript.parser.PSTokens.Companion.LDARROW
+import org.purescript.parser.PSTokens.Companion.MLCOMMENT
+import org.purescript.parser.PSTokens.Companion.MODULE_PREFIX
+import org.purescript.parser.PSTokens.Companion.NATURAL
+import org.purescript.parser.PSTokens.Companion.OPERATOR
+import org.purescript.parser.PSTokens.Companion.OPTIMISTIC
+import org.purescript.parser.PSTokens.Companion.PIPE
+import org.purescript.parser.PSTokens.Companion.PROPER_NAME
+import org.purescript.parser.PSTokens.Companion.SEMI
+import org.purescript.parser.PSTokens.Companion.SLCOMMENT
+import org.purescript.parser.PSTokens.Companion.STRING
+import org.purescript.parser.PSTokens.Companion.STRING_ERROR
+import org.purescript.parser.PSTokens.Companion.STRING_ESCAPED
+import org.purescript.parser.PSTokens.Companion.STRING_GAP
+import org.purescript.psi.PSForeignValueDeclaration
+import org.purescript.psi.PSModule
+import org.purescript.psi.PSVarBinderImpl
 import org.purescript.psi.classes.PSClassDeclaration
 import org.purescript.psi.classes.PSClassMember
 import org.purescript.psi.data.PSDataConstructor
 import org.purescript.psi.data.PSDataDeclaration
+import org.purescript.psi.declaration.PSFixityDeclaration
 import org.purescript.psi.declaration.PSValueDeclaration
 import org.purescript.psi.imports.PSImportAlias
 import org.purescript.psi.newtype.PSNewTypeConstructor
@@ -28,6 +62,42 @@ class PSFindUsageProvider : FindUsagesProvider {
             || psiElement is PSClassDeclaration
             || psiElement is PSTypeSynonymDeclaration
             || psiElement is PSClassMember
+            || psiElement is PSFixityDeclaration
+
+    override fun getWordsScanner(): WordsScanner {
+        val psWordScanner = DefaultWordsScanner(
+            PSLexer(),
+            TokenSet.create(IDENT, PROPER_NAME, MODULE_PREFIX),
+            TokenSet.create(MLCOMMENT, SLCOMMENT, DOC_COMMENT),
+            TokenSet.create(
+                STRING,
+                STRING_ERROR,
+                STRING_ESCAPED,
+                STRING_GAP,
+                CHAR,
+                NATURAL,
+                FLOAT
+            ),
+            TokenSet.EMPTY,
+            TokenSet.create(
+                OPERATOR,
+                ARROW,
+                BACKSLASH, // maybe?!
+                COMMA,
+                DARROW,
+                DCOLON,
+                DDOT,
+                DOT,
+                EQ, // maybe?!
+                LARROW,
+                LDARROW,
+                OPTIMISTIC,
+                PIPE,
+                SEMI,
+            )
+        )
+        return psWordScanner
+    }
 
     override fun getHelpId(psiElement: PsiElement): String? {
         return null
@@ -47,6 +117,7 @@ class PSFindUsageProvider : FindUsagesProvider {
             is PSForeignValueDeclaration -> "foreign value"
             is PSTypeSynonymDeclaration -> "type synonym"
             is PSClassMember -> "class member"
+            is PSFixityDeclaration -> "operator"
             else -> "unknown"
         }
     }
