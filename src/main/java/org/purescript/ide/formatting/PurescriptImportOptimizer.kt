74ea52dcd7141f70cd0881bd2aefaf0e6529e0d5
Simon Olander <simon.olander@gmail.com>
2021-05-15 17:48:11 +0200

    Refactor import optimizer

--- src/main/java/org/purescript/ide/formatting/PurescriptImportOptimizer.kt
+++ src/main/java/org/purescript/ide/formatting/PurescriptImportOptimizer.kt
@@ -14,30 +14,31 @@ class PurescriptImportOptimizer : ImportOptimizer {
         val psFile = file as PSFile
         val module = psFile.module ?: error("File ${file.name} contains no Purescript module")
         val factory = PSPsiFactory(file.project)
-        val importDeclarations = module.importDeclarations.map { importDeclaration ->
-            ImportDeclaration(
-                importDeclaration.moduleName!!.name,
-                importDeclaration.isHiding,
-                importDeclaration.importedItems.map { importedItem ->
-                    when (importedItem) {
-                        is PSImportedClass -> ImportedClass(importedItem.name)
-                        is PSImportedKind -> ImportedKind(importedItem.name)
-                        is PSImportedType -> ImportedType(importedItem.name)
-                        is PSImportedData -> ImportedData(
-                            importedItem.name,
-                            importedItem.importsAll,
-                            importedItem.importedDataMembers.map { it.name }.toSet()
-                        )
-                        is PSImportedValue -> ImportedValue(importedItem.name)
-                        is PSImportedOperator -> ImportedOperator(importedItem.name)
-                    }
-                }.toSet(),
-                alias = importDeclaration.importAlias?.name
-            )
-        }.toSet()
-
+        val importDeclarations = module.importDeclarations.map(::fromPsiElement).toSet()
         val mergedImportDeclarations = mergeImportDeclarations(importDeclarations)
             .map { factory.createImportDeclaration(it)!! }
         return Runnable { module.replaceImportDeclarations(mergedImportDeclarations) }
     }
+
+    private fun fromPsiElement(importDeclaration: PSImportDeclarationImpl): ImportDeclaration =
+        ImportDeclaration(
+            importDeclaration.moduleName!!.name,
+            importDeclaration.isHiding,
+            importDeclaration.importedItems.map(::fromPsiElement).toSet(),
+            importDeclaration.importAlias?.name
+        )
+
+    private fun fromPsiElement(importedItem: PSImportedItem): ImportedItem =
+        when (importedItem) {
+            is PSImportedClass -> ImportedClass(importedItem.name)
+            is PSImportedKind -> ImportedKind(importedItem.name)
+            is PSImportedType -> ImportedType(importedItem.name)
+            is PSImportedData -> ImportedData(
+                importedItem.name,
+                importedItem.importsAll,
+                importedItem.importedDataMembers.map { it.name }.toSet()
+            )
+            is PSImportedValue -> ImportedValue(importedItem.name)
+            is PSImportedOperator -> ImportedOperator(importedItem.name)
+        }
 }
