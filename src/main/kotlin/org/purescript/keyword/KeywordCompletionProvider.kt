3e14d212e1a0575f981892462f2e6b0fba18b3ec
Samuel Ytterbrink <neppord@gmail.com>
2021-12-25 19:53:38 +0100

    Add keyword autocomplete with do as first example

---
+++ src/main/kotlin/org/purescript/keyword/KeywordCompletionProvider.kt
@@ -0,0 +1,17 @@
+package org.purescript.keyword
+
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.util.ProcessingContext
+
+class KeywordCompletionProvider : CompletionProvider<CompletionParameters>() {
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
+        result.addElement(LookupElementBuilder.create("do"))
+    }
+}
