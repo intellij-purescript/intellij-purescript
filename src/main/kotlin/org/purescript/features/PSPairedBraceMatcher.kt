d03bcdda899f42766efe9a3d00aaa5d9299dba16
Samuel Ytterbrink <neppord@gmail.com>
2022-01-16 11:33:26 +0100

    Simplify auto translated code java -> kotlin

--- src/main/kotlin/org/purescript/features/PSPairedBraceMatcher.kt
+++ src/main/kotlin/org/purescript/features/PSPairedBraceMatcher.kt
@@ -7,30 +7,19 @@ import com.intellij.psi.tree.IElementType
 import org.purescript.parser.PSTokens
 
 class PSPairedBraceMatcher : PairedBraceMatcher {
-    override fun getPairs(): Array<BracePair> {
-        return PAIRS
-    }
+    override fun getPairs() = PAIRS
 
     override fun isPairedBracesAllowedBeforeType(
         lbraceType: IElementType,
         contextType: IElementType?
-    ): Boolean {
-        return (contextType == null
-                || contextType === PSTokens.WS)
-    }
+    ) = contextType == null || contextType === PSTokens.WS
 
-    override fun getCodeConstructStart(
-        file: PsiFile,
-        openingBraceOffset: Int
-    ): Int {
-        return openingBraceOffset
-    }
-
-    companion object {
-        private val PAIRS = arrayOf(
-            BracePair(PSTokens.LCURLY, PSTokens.RCURLY, true),
-            BracePair(PSTokens.LBRACK, PSTokens.RBRACK, true),
-            BracePair(PSTokens.LPAREN, PSTokens.RPAREN, false)
-        )
-    }
+    override fun getCodeConstructStart(file: PsiFile, openingBraceOffset: Int) =
+        openingBraceOffset
 }
+
+private val PAIRS = arrayOf(
+    BracePair(PSTokens.LCURLY, PSTokens.RCURLY, true),
+    BracePair(PSTokens.LBRACK, PSTokens.RBRACK, true),
+    BracePair(PSTokens.LPAREN, PSTokens.RPAREN, false)
+)
