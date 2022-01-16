e8ff82e69d299e692060f2d56a3ea3321be34f6b
Samuel Ytterbrink <neppord@gmail.com>
2022-01-16 11:53:27 +0100

    Simplify auto translated code java -> kotlin

--- src/main/kotlin/org/purescript/file/PSFile.kt
+++ src/main/kotlin/org/purescript/file/PSFile.kt
@@ -1,7 +1,6 @@
 package org.purescript.file
 
 import com.intellij.extapi.psi.PsiFileBase
-import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.FileViewProvider
 import org.purescript.PSLanguage
 import org.purescript.psi.PSModule
@@ -9,23 +8,15 @@ import org.purescript.psi.declaration.PSValueDeclaration
 
 class PSFile(viewProvider: FileViewProvider) :
     PsiFileBase(viewProvider, PSLanguage.INSTANCE) {
-    override fun getFileType(): FileType {
-        return PS_FILE_TYPE_INSTANCE
-    }
-
-    override fun toString(): String {
-        return "Purescript File"
-    }
+    override fun getFileType() = PS_FILE_TYPE_INSTANCE
+    override fun toString() = "Purescript File"
 
     /**
      * @return the [PSModule] that this file contains,
      * or null if the module couldn't be parsed
      */
-    val module: PSModule?
-        get() = findChildByClass(PSModule::class.java)
-
+    val module: PSModule? get() = findChildByClass(PSModule::class.java)
     val topLevelValueDeclarations: Map<String, List<PSValueDeclaration>>
         get() = module?.valueDeclarations?.groupBy { it.name }
             ?: emptyMap()
-
 }
