cc4ca3aa931bb4c622f739e7b68a51686379aa70
neppord <neppord@gmail.com>
2022-12-14 15:30:09 +0100

    Allow operators to be renamed in place

--- src/main/kotlin/org/purescript/ide/refactoring/PSRefactoringSupportProvider.kt
+++ src/main/kotlin/org/purescript/ide/refactoring/PSRefactoringSupportProvider.kt
@@ -4,11 +4,13 @@ import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
 import org.purescript.psi.PSModule
 import org.purescript.psi.binder.PSVarBinder
+import org.purescript.psi.declaration.PSFixityDeclaration
 import org.purescript.psi.declaration.PSValueDeclaration
 
 class PSRefactoringSupportProvider : RefactoringSupportProvider() {
     override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean =
         element is PSModule ||
             element is PSValueDeclaration ||
+            element is PSFixityDeclaration ||
             element is PSVarBinder
 }
