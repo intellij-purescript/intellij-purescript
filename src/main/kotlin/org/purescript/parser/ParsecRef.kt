731ebc3d6b189a580e76b63e26bc07c8c87a7ef3
neppord <neppord@gmail.com>
2022-12-21 21:19:23 +0100

    Format

--- src/main/kotlin/org/purescript/parser/ParsecRef.kt
+++ src/main/kotlin/org/purescript/parser/ParsecRef.kt
@@ -8,7 +8,6 @@ class ParsecRef(private val init: Parsec.() -> Parsec) : Parsec() {
     override fun parse(context: ParserContext): ParserInfo = ref.parse(context)
     public override fun calcName() = ref.name!!
     override fun calcExpectedName() = ref.expectedName!!
-    override val canStartWithSet: TokenSet
-        get() = ref.canStartWithSet
+    override val canStartWithSet: TokenSet get() = ref.canStartWithSet
     public override fun calcCanBeEmpty() = ref.canBeEmpty
 }
