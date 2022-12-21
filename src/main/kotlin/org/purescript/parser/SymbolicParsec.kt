8b886575810efcaeab8f3c20746ee9abca3216b2
neppord <neppord@gmail.com>
2022-12-21 21:21:01 +0100

    Transform var to val

--- src/main/kotlin/org/purescript/parser/SymbolicParsec.kt
+++ src/main/kotlin/org/purescript/parser/SymbolicParsec.kt
@@ -8,16 +8,17 @@ class SymbolicParsec(private val ref: Parsec, private val node: IElementType) :
     override fun parse(context: ParserContext): ParserInfo {
         val startPosition = context.position
         val pack = context.start()
-        var info = ref.parse(context)
+        val info = ref.parse(context)
         if (info.success) {
             pack.done(node)
         } else {
             pack.drop()
         }
-        if (startPosition == info.position) {
-            info = ParserInfo(info.position, this, info.success)
+        return if (startPosition == info.position) {
+            ParserInfo(info.position, this, info.success)
+        } else {
+            info
         }
-        return info
     }
     public override fun calcName() = node.toString()
     override fun calcExpectedName() = setOf(node.toString())
