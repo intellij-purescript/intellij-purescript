32eaa4dc47146b817316a9c06d938303a10e69bc
neppord <neppord@gmail.com>
2022-12-22 14:24:58 +0100

    Format

--- src/main/kotlin/org/purescript/parser/ParserContext.kt
+++ src/main/kotlin/org/purescript/parser/ParserContext.kt
@@ -9,15 +9,12 @@ class ParserContext(private val builder: PsiBuilder) {
     var isInAttempt = false
     private var inOptional = 0
     fun eof() = builder.eof()
-
     fun advance() = builder.advanceLexer()
-
     fun enterOptional() = inOptional++
     fun exitOptional() = inOptional--
     fun isInOptional() = inOptional > 0
     fun text() = builder.tokenText ?: ""
     fun peek() = builder.tokenType ?: EOF
-
     fun eat(type: IElementType): Boolean {
         if (builder.tokenType === type) {
             advance()
@@ -25,12 +22,8 @@ class ParserContext(private val builder: PsiBuilder) {
         }
         return false
     }
-
     fun start(): PsiBuilder.Marker = builder.mark()
-
-    val position: Int
-        get() = builder.currentOffset
-
+    val position: Int get() = builder.currentOffset
     fun getText(start: Int, end: Int) =
         builder.originalText.subSequence(start, end).toString()
 }
