08983aabb13e80c6dc92b72f379eae5a0b8c184b
neppord <neppord@gmail.com>
2022-12-23 14:27:47 +0100

    Simplify parser

--- src/main/kotlin/org/purescript/parser/NoneOrMoreParser.kt
+++ src/main/kotlin/org/purescript/parser/NoneOrMoreParser.kt
@@ -3,35 +3,13 @@ package org.purescript.parser
 import com.intellij.psi.tree.TokenSet
 
 class NoneOrMoreParser(private val p: Parsec) : Parsec() {
-    override fun parse(context: ParserContext): ParserInfo {
-        var info = ParserInfo(context.position, setOf(p), true)
-        while (!context.eof()) {
-            val position = context.position
-            info = p.parse(context)
-            if (info.success) {
-                if (position == context.position) {
-                    // TODO: this should not be allowed.
-                    val info2 = ParserInfo(
-                        context.position,
-                        info.expected,
-                        false
-                    )
-                    return info.merge(info2)
-                }
-            } else {
-                return if (position == context.position) {
-                    val info2 = ParserInfo(
-                        context.position,
-                        info.expected,
-                        true
-                    )
-                    info.merge(info2)
-                } else {
-                    info
-                }
-            }
+    override tailrec fun parse(context: ParserContext): ParserInfo {
+        val position = context.position
+        val info = p.parse(context)
+        return when (info.success) {
+            false -> ParserInfo(position, setOf(p), true)
+            true -> parse(context)
         }
-        return info
     }
 
     public override fun calcName() = "(" + p.name + ")*"
