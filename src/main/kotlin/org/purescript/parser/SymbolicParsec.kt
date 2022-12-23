999fd5f2be78ec84ba488ff29380cd554f8faf45
Samuel Ytterbrink <neppord@gmail.com>
2022-12-23 18:01:04 +0100

    Remove line that did nothing

--- src/main/kotlin/org/purescript/parser/SymbolicParsec.kt
+++ src/main/kotlin/org/purescript/parser/SymbolicParsec.kt
@@ -7,7 +7,6 @@ import org.purescript.parser.Info.Failure
 class SymbolicParsec(private val ref: Parsec, private val node: IElementType) :
     Parsec() {
     override fun parse(context: ParserContext): Info {
-        context.position
         val pack = context.start()
         val info = ref.parse(context)
         if (info !is Failure) {
