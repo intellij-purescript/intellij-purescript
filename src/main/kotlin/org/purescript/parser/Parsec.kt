d58796a2e9b1fa1d15f4bb296da9453a19802e5d
neppord <neppord@gmail.com>
2022-12-25 12:11:45 +0100

    Remove unused method

--- src/main/kotlin/org/purescript/parser/Parsec.kt
+++ src/main/kotlin/org/purescript/parser/Parsec.kt
@@ -9,7 +9,6 @@ abstract class Parsec {
     abstract val canStartWithSet: TokenSet
     abstract fun parse(context: ParserContext): Info
     operator fun plus(other: Parsec) = SeqParser(arrayOf(other), this)
-    fun or(next: Parsec) = ChoiceParser(this, arrayOf(next))
     fun noneOrMore() = NoneOrMoreParser(this)
     infix fun `as`(node: IElementType) = SymbolicParsec(this, node)
     fun withRollback() = RollbackParser(this)
