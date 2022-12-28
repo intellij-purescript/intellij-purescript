15b409e93e48689ad32a521bfadbe16fafefdbde
neppord <neppord@gmail.com>
2022-12-28 16:23:52 +0100

    Move Fixity stub into Fixity interface

--- src/main/kotlin/org/purescript/psi/declaration/FixityDeclaration.kt
+++ src/main/kotlin/org/purescript/psi/declaration/FixityDeclaration.kt
@@ -4,26 +4,23 @@ import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.psi.stubs.*
-import org.purescript.psi.PSElementType
+import org.purescript.psi.PSElementType.WithPsiAndStub
 import org.purescript.psi.PSPsiFactory
 import org.purescript.psi.PSStubbedElement
 import org.purescript.psi.module.Module
 import org.purescript.psi.name.PSOperatorName
 
 interface FixityDeclaration {
-    object Type : PSElementType.WithPsiAndStub<PSFixityDeclarationStub,
-        Psi>("FixityDeclaration") {
+    class Stub(val name: String, parent: StubElement<*>?) :
+        StubBase<Psi>(parent, Type), StubElement<Psi>
+
+    object Type : WithPsiAndStub<Stub, Psi>("FixityDeclaration") {
         override fun createPsi(node: ASTNode) = Psi(node)
-        override fun createPsi(stub: PSFixityDeclarationStub) =
-            Psi(stub, this)
+        override fun createPsi(stub: Stub) = Psi(stub, this)
+        override fun createStub(psi: Psi, p: StubElement<*>?) =
+            Stub(psi.name, p)
 
-        override fun createStub(
-            psi: Psi,
-            parent: StubElement<*>?
-        ) =
-            PSFixityDeclarationStub(psi.name, parent)
-
-        override fun indexStub(stub: PSFixityDeclarationStub, sink: IndexSink) {
+        override fun indexStub(stub: Stub, sink: IndexSink) {
             // if there is a parser error the module might not exist
             stub.getParentStubOfType(Module.Psi::class.java)?.let { module ->
                 // TODO only index exported declarations
@@ -34,37 +31,27 @@ interface FixityDeclaration {
             }
         }
 
-        override fun serialize(
-            stub: PSFixityDeclarationStub,
-            data: StubOutputStream
-        ) {
-            data.writeName(stub.name)
-        }
+        override fun serialize(stub: Stub, d: StubOutputStream) =
+            d.writeName(stub.name)
 
-        override fun deserialize(
-            dataStream: StubInputStream,
-            parent: StubElement<*>?
-        ): PSFixityDeclarationStub =
-            PSFixityDeclarationStub(dataStream.readNameString()!!, parent)
-
+        override fun deserialize(d: StubInputStream, p: StubElement<*>?): Stub =
+            Stub(d.readNameString()!!, p)
     }
-    class Psi : PSStubbedElement<PSFixityDeclarationStub>,
-        PsiNameIdentifierOwner {
 
+    class Psi : PSStubbedElement<Stub>, PsiNameIdentifierOwner {
         constructor(node: ASTNode) : super(node)
+        constructor(stub: Stub, type: IStubElementType<*, *>) :
+            super(stub, type)
 
-        constructor(stub: PSFixityDeclarationStub, type: IStubElementType<*, *>)
-            : super(stub, type)
+        // Todo clean this up
+        override fun toString(): String = "PSFixityDeclaration($elementType)"
 
         private val operatorName
             get() = findNotNullChildByClass(PSOperatorName::class.java)
 
         override fun getTextOffset(): Int = nameIdentifier.textOffset
-
         override fun getNameIdentifier() = operatorName
-
         override fun getName() = stub?.name ?: operatorName.name
-
         override fun setName(name: String): PsiElement? {
             val identifier = PSPsiFactory(project).createOperatorName(name)
                 ?: return null
