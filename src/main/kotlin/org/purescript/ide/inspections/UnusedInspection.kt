cdd6a2a56722427de7c1f2aea2bcca276373d91e
neppord <neppord@gmail.com>
2023-01-08 15:45:05 +0100

    Add safe delete inspection

---
+++ src/main/kotlin/org/purescript/ide/inspections/UnusedInspection.kt
@@ -0,0 +1,38 @@
+package org.purescript.ide.inspections
+
+import com.intellij.codeInspection.*
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.search.searches.ReferencesSearch.search
+import com.intellij.refactoring.safeDelete.SafeDeleteHandler
+import org.purescript.psi.declaration.PSSignature
+import org.purescript.psi.declaration.PSValueDeclaration
+
+class UnusedInspection : LocalInspectionTool() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) =
+        Visitor(holder)
+
+    class Visitor(private val holder: ProblemsHolder) : PsiElementVisitor() {
+        override fun visitElement(element: PsiElement) = when (element) {
+            is PSValueDeclaration -> when {
+                element.name == "main" -> Unit
+                search(element).anyMatch { it.element !is PSSignature } -> Unit
+                else -> holder.registerProblem(
+                    element.nameIdentifier,
+                    "Unused value declaration",
+                    ProblemHighlightType.LIKE_UNUSED_SYMBOL,
+                    SafeDelete(element)
+                )
+            }
+            else -> Unit
+        }
+
+    }
+
+    class SafeDelete(private val element: PsiElement) : LocalQuickFix {
+        override fun getFamilyName() = "Safe delete"
+        override fun applyFix(project: Project, descriptor: ProblemDescriptor) =
+            SafeDeleteHandler.invoke(project, arrayOf(element), false)
+    }
+}
