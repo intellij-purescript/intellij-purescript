0ca2c08464c777ab333e19d1336d3b092c33d0a5
neppord <neppord@gmail.com>
2023-01-10 16:34:35 +0100

    Remove unused psi classes and their element types

--- src/main/kotlin/org/purescript/psi/PSRowKind.kt
+++
@@ -1,6 +0,0 @@
-package org.purescript.psi
-
-import com.intellij.lang.ASTNode
-import org.purescript.psi.base.PSPsiElement
-
-class PSRowKind(node: ASTNode) : PSPsiElement(node)
--- src/main/kotlin/org/purescript/psi/PSStar.kt
+++
@@ -1,6 +0,0 @@
-package org.purescript.psi
-
-import com.intellij.lang.ASTNode
-import org.purescript.psi.base.PSPsiElement
-
-class PSStar(node: ASTNode) : PSPsiElement(node)
