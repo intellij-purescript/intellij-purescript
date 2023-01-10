e2333d444880ece28ac9bfbe9ea6966b36246fc5
neppord <neppord@gmail.com>
2023-01-10 16:26:01 +0100

    Group more type psi elements

--- src/main/kotlin/org/purescript/psi/type/PSObjectType.kt
+++ src/main/kotlin/org/purescript/psi/type/PSObjectType.kt
@@ -1,5 +1,6 @@
-package org.purescript.psi
+package org.purescript.psi.type
 
 import com.intellij.lang.ASTNode
+import org.purescript.psi.PSPsiElement
 
 class PSObjectType(node: ASTNode) : PSPsiElement(node)
