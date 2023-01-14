c95c2006ab40cac9165c3fa136d629908a1d43cf
neppord <neppord@gmail.com>
2023-01-14 20:06:16 +0100

    Move value into expression

--- src/main/kotlin/org/purescript/psi/expression/PSValue.kt
+++ src/main/kotlin/org/purescript/psi/expression/PSValue.kt
@@ -2,7 +2,6 @@ package org.purescript.psi.expression
 
 import com.intellij.lang.ASTNode
 import org.purescript.psi.base.PSPsiElement
-import org.purescript.psi.expression.ExpressionAtom
 
 class PSValue(node: ASTNode) : PSPsiElement(node) {
     val expressionAtoms
