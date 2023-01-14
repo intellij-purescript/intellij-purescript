d5db8a7129952b1bbcb05c46a16f29c27da1821e
neppord <neppord@gmail.com>
2023-01-14 20:03:40 +0100

    Remove dead code

--- src/main/kotlin/org/purescript/psi/PSValue.kt
+++ src/main/kotlin/org/purescript/psi/PSValue.kt
@@ -3,14 +3,8 @@ package org.purescript.psi
 import com.intellij.lang.ASTNode
 import org.purescript.psi.base.PSPsiElement
 import org.purescript.psi.expression.ExpressionAtom
-import org.purescript.psi.expression.PSExpressionIdentifier
-import org.purescript.psi.expression.PSExpressionOperator
 
 class PSValue(node: ASTNode) : PSPsiElement(node) {
     val expressionAtoms
         get() = findChildrenByClass(ExpressionAtom::class.java).toList()
-    val expressionOperators 
-        get() = findChildrenByClass(PSExpressionOperator::class.java).toList()
-    val expressionIdentifiers: List<PSExpressionIdentifier>
-        get() = findChildrenByClass(PSExpressionIdentifier::class.java).toList()
 }
