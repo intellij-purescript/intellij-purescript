4eb7f21f9d856a23a87dea9df91270d0a1a75a2a
neppord <neppord@gmail.com>
2023-01-19 21:22:26 +0100

    Fix working directory for spago run anything

--- src/main/kotlin/org/purescript/run/spago/SpagoRunAnythingProvider.kt
+++ src/main/kotlin/org/purescript/run/spago/SpagoRunAnythingProvider.kt
@@ -1,12 +1,24 @@
 package org.purescript.run.spago
 
+import com.intellij.ide.actions.runAnything.RunAnythingAction
+import com.intellij.ide.actions.runAnything.RunAnythingUtil
 import com.intellij.ide.actions.runAnything.activity.RunAnythingCommandProvider
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.util.SystemInfo
 import org.purescript.icons.PSIcons
-import javax.swing.Icon
 
 class SpagoRunAnythingProvider : RunAnythingCommandProvider() {
+    override fun execute(dataContext: DataContext, value: String) {
+        val workDirectory = dataContext.getData(CommonDataKeys.PROJECT)
+            ?.guessProjectDir()
+        val executor = dataContext.getData(RunAnythingAction.EXECUTOR_KEY)
+        RunAnythingUtil.LOG.assertTrue(workDirectory != null)
+        RunAnythingUtil.LOG.assertTrue(executor != null)
+        runCommand(workDirectory!!, value, executor!!, dataContext)
+    }
+
     override fun getIcon(value: String) = PSIcons.SPAGO
     override fun findMatchingValue(
         dataContext: DataContext,
