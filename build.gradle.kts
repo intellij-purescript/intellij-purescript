06c7f11c069597481546eaefea380b5fcfe18e28
neppord <neppord@gmail.com>
2023-01-21 14:15:31 +0100

    Remove explicit dependency on kotlin stdlib

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,14 +28,11 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
     testImplementation("junit:junit:4.13.2")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
 }
 
 java {
