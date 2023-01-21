52c5ab9b6b225ee27fe7666bce923be73c92b68e
neppord <neppord@gmail.com>
2023-01-21 11:54:44 +0100

    Fix deprecation warning

--- src/main/kotlin/org/purescript/PurescriptLiveTemplateContext.kt
+++ src/main/kotlin/org/purescript/PurescriptLiveTemplateContext.kt
@@ -3,7 +3,7 @@ package org.purescript
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-class PurescriptLiveTemplateContext : TemplateContextType("PURESCRIPT", "Purescript") {
+class PurescriptLiveTemplateContext : TemplateContextType("Purescript") {
     override fun isInContext(templateActionContext: TemplateActionContext) =
         templateActionContext.file.name.endsWith(".purs")
 }
