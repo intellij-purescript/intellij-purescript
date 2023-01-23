d8de4f85c6388089ab1676b7b4403a9f609d6f08
neppord <neppord@gmail.com>
2023-01-23 20:07:43 +0100

    Cleanup

--- src/main/kotlin/org/purescript/lexer/LayoutState.kt
+++ src/main/kotlin/org/purescript/lexer/LayoutState.kt
@@ -1,6 +1,6 @@
 package org.purescript.lexer
 
-import org.purescript.lexer.LayoutDelimiter.CaseBinders
+import org.purescript.lexer.LayoutDelimiter.*
 import org.purescript.lexer.token.SourcePos
 import org.purescript.parser.LAYOUT_END
 import org.purescript.parser.LAYOUT_SEP
@@ -10,8 +10,8 @@ data class LayoutState(
     val stack: LayoutStack?,
     val acc: List<Pair<SuperToken, LayoutStack?>>
 ) {
-    inline fun collapse(tokPos: SourcePos, p: (LayoutDelimiter) -> Boolean)
-        : LayoutState = this.collapse(tokPos) { _, _, lyt -> p(lyt) }
+    inline fun collapse(tokPos: SourcePos, p: (LayoutDelimiter) -> Boolean) =
+        collapse(tokPos) { _, _, lyt -> p(lyt) }
 
     fun insertStart(nextPos: SourcePos, lyt: LayoutDelimiter): LayoutState =
         when (val indent = stack?.find { it.isIndent }) {
@@ -24,18 +24,20 @@ data class LayoutState(
                     .insertToken(lytToken(nextPos, LAYOUT_START))
             }
         }
+
     inline fun insertKwProperty(
         src: SuperToken,
         tokPos: SourcePos,
         k: (LayoutState) -> LayoutState
     ): LayoutState {
         val state2 = insertDefault(src, tokPos)
-        return if (stack?.layoutDelimiter == LayoutDelimiter.Property) {
-            LayoutState(state2.stack?.tail, state2.acc)
+        return if (stack?.layoutDelimiter == Property) {
+            state2.popStack()
         } else {
             k(state2)
         }
     }
+
     inline fun collapse(
         tokPos: SourcePos,
         p: (SourcePos, SourcePos, LayoutDelimiter) -> Boolean
@@ -46,23 +48,21 @@ data class LayoutState(
             p(tokPos, stack.sourcePos, stack.layoutDelimiter)
         ) {
             if (stack.layoutDelimiter.isIndent) {
-                val pair = lytToken(tokPos, LAYOUT_END) to stack.tail
-                acc = acc.toMutableList() + pair
+                acc = acc + (lytToken(tokPos, LAYOUT_END) to stack.tail)
             }
             stack = stack.tail
         }
         return LayoutState(stack, acc)
     }
 
-    fun insertToken(token: SuperToken) =
-        copy(acc = acc.toMutableList() + (token to stack))
-
+    fun insertToken(token: SuperToken) = copy(acc = acc + (token to stack))
     fun pushStack(lytPos: SourcePos, lyt: LayoutDelimiter) =
         copy(stack = LayoutStack(lytPos, lyt, stack))
 
     fun insertDefault(src: SuperToken, tokPos: SourcePos) =
         collapse(tokPos, ::offsideP).insertSep(tokPos).insertToken(src)
 
+    fun popStack() = popStack { true }
     inline fun popStack(p: (LayoutDelimiter) -> Boolean): LayoutState = when {
         stack == null -> this
         p(stack.layoutDelimiter) -> copy(stack = stack.tail)
@@ -70,19 +70,17 @@ data class LayoutState(
     }
 
     fun insertSep(tokPos: SourcePos): LayoutState {
-        val (lytPos, lyt, tail) = this.stack ?: return this
+        val (_, lyt, _) = stack ?: return this
         val sepTok = lytToken(tokPos, LAYOUT_SEP)
         return when {
-            LayoutDelimiter.TopDecl == lyt && sepP(tokPos, lytPos) ->
-                copy(stack = tail).insertToken(sepTok)
+            TopDecl == lyt && sepP(tokPos, stack.sourcePos) ->
+                popStack().insertToken(sepTok)
 
-            LayoutDelimiter.TopDeclHead == lyt && sepP(tokPos, lytPos) ->
-                copy(stack = tail).insertToken(sepTok)
+            TopDeclHead == lyt && sepP(tokPos, stack.sourcePos) ->
+                popStack().insertToken(sepTok)
 
-            lyt.isIndent && sepP(tokPos, lytPos) -> when (lyt) {
-                LayoutDelimiter.Of ->
-                    insertToken(sepTok).pushStack(tokPos, CaseBinders)
-
+            lyt.isIndent && sepP(tokPos, stack.sourcePos) -> when (lyt) {
+                Of -> insertToken(sepTok).pushStack(tokPos, CaseBinders)
                 else -> insertToken(sepTok)
             }
 
