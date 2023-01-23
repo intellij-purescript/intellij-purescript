94e06d318a8584d242e79eab36cfd87afbf18733
neppord <neppord@gmail.com>
2023-01-23 15:43:45 +0100

    Remove method bump that only had tests

--- src/main/kotlin/org/purescript/lexer/token/SourcePos.kt
+++ src/main/kotlin/org/purescript/lexer/token/SourcePos.kt
@@ -4,24 +4,4 @@ data class SourcePos(
     val line: Int,
     val column: Int,
     val offset: Int,
-) {
-    fun bump(text: CharSequence, nextOffset: Int): SourcePos {
-        val subSequence = text.subSequence(offset, nextOffset)
-        return when {
-            "\n" in subSequence -> {
-                SourcePos(
-                    line = line + subSequence.count { it == '\n'},
-                    column = nextOffset - text.lastIndexOf("\n", nextOffset) - 1,
-                    offset = nextOffset
-                )
-            }
-            else -> {
-                SourcePos(
-                    line = line,
-                    column = column + nextOffset,
-                    offset = nextOffset
-                )
-            }
-        }
-    }
-}
+) 
