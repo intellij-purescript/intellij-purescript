56191010c788ad0c06738c1c737a240829c84270
neppord <neppord@gmail.com>
2023-02-05 16:50:27 +0100

    Add comment

--- src/main/kotlin/org/purescript/psi/declaration/imports/ReExportedImportIndex.kt
+++ src/main/kotlin/org/purescript/psi/declaration/imports/ReExportedImportIndex.kt
@@ -3,6 +3,9 @@ package org.purescript.psi.declaration.imports
 import com.intellij.psi.stubs.StringStubIndexExtension
 import com.intellij.psi.stubs.StubIndexKey.createIndexKey
 
+/**
+ * Maps Module names to Import Statements that imports that module and also are exported by their own module
+ */
 object ReExportedImportIndex : StringStubIndexExtension<Import>() {
     val KEY = createIndexKey<String, Import>("purescript.reexported.import")
 
