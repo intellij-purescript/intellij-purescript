f10336a1c44bd8543b7bee5a35e323bf50cc9a35
neppord <neppord@gmail.com>
2023-02-06 15:19:20 +0100

    Add more tests to exporession tree

--- src/test/kotlin/org/purescript/psi/expression/TestTree.kt
+++ src/test/kotlin/org/purescript/psi/expression/TestTree.kt
@@ -8,15 +8,18 @@ sealed interface Tree {
     val ranges: Sequence<TextRange>
     val start: Int
     val end: Int
+    fun add(node: Tree): Tree
 
     data class Span(override val start: Int, override val end: Int) : Tree {
         override val ranges get() = emptySequence<TextRange>()
+        override fun add(node: Tree): Tree = Call(this, node)
     }
 
     data class Call(val function: Tree, val argument: Tree) : Tree {
         override val ranges get() = sequenceOf(TextRange(start, end))
         override val start get() = function.start
         override val end get() = argument.end
+        override fun add(node: Tree): Tree = Call(this, node)
     }
 
     data class Operator(
@@ -28,6 +31,9 @@ sealed interface Tree {
         override val ranges get() = sequenceOf(TextRange(start, end))
         override val start: Int get() = left.start
         override val end: Int get() = right.end
+        override fun add(node: Tree): Tree {
+            TODO("Not yet implemented")
+        }
 
     }
 
@@ -35,6 +41,7 @@ sealed interface Tree {
         override val ranges get() = emptySequence<TextRange>()
         override val start: Int get() = TODO("Not yet implemented")
         override val end: Int get() = TODO("Not yet implemented")
+        override fun add(node: Tree): Tree = node
     }
 }
 
@@ -56,5 +63,25 @@ class TestTree : TestCase() {
     fun `test operator has range including both children`() {
         val call = Tree.Operator(Associativity.Infix, 0, Tree.Span(1, 2), Tree.Span(2, 3))
         assertEquals(TextRange(1, 3), call.ranges.single())
+    }
+    
+    fun `test adding anything to empty replaces it `() {
+        val span = Tree.Span(1, 2)
+        val tree = Tree.Empty.add(span)
+        assertEquals(span, tree)
+    }
+    
+    fun `test adding spans together creates call`() {
+        val f = Tree.Span(1, 2)
+        val a = Tree.Span(2, 3)
+        val tree = Tree.Empty.add(f).add(a)
+        assertEquals(Tree.Call(f, a), tree)
+    }
+    fun `test adding spans together to call creates nesting call`() {
+        val f = Tree.Span(1, 2)
+        val a = Tree.Span(2, 3)
+        val b = Tree.Span(3, 4)
+        val tree = Tree.Empty.add(f).add(a).add(b)
+        assertEquals(Tree.Call(Tree.Call(f, a), b), tree)
     }
 }
