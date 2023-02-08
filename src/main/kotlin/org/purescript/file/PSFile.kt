334338ec983a2f9bd8717eb34cba32e3322dbd64
neppord <neppord@gmail.com>
2023-02-08 14:06:22 +0100

    Bump stub file version

--- src/main/kotlin/org/purescript/file/PSFile.kt
+++ src/main/kotlin/org/purescript/file/PSFile.kt
@@ -21,7 +21,7 @@ class PSFile(viewProvider: FileViewProvider) :
     }
 
     object Type : IStubFileElementType<Stub>("PSFile", PSLanguage) {
-        override fun getStubVersion(): Int = 1
+        override fun getStubVersion(): Int = 2
         override fun getBuilder(): StubBuilder = object : DefaultStubBuilder() {
             override fun createStubForFile(file: PsiFile): Stub = Stub(file as PSFile)
         }
