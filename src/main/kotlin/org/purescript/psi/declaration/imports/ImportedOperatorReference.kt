44b5eaf517ff9451e55f8507b26e311b05162d0b
neppord <neppord@gmail.com>
2023-03-01 20:01:42 +0100

    Format

--- src/main/kotlin/org/purescript/psi/declaration/imports/ImportedOperatorReference.kt
+++ src/main/kotlin/org/purescript/psi/declaration/imports/ImportedOperatorReference.kt
@@ -5,26 +5,10 @@ import com.intellij.psi.PsiReferenceBase
 import org.purescript.psi.PSPsiFactory
 
 class ImportedOperatorReference(element: PSImportedOperator) :
-    PsiReferenceBase<PSImportedOperator>(
-        element,
-        element.symbol.operator.textRangeInParent,
-        false
-    ) {
-
-    override fun getVariants(): Array<Any> =
-        candidates.toTypedArray()
-
-    override fun resolve(): PsiElement? {
-        return candidates.firstOrNull { it.name == element.name }
-    }
-
-    val candidates
-        get() =
-            element
-                .importDeclaration
-                ?.importedModule
-                ?.exportedFixityDeclarations
-                ?: listOf()
+    PsiReferenceBase<PSImportedOperator>(element, element.symbol.operator.textRangeInParent, false) {
+    override fun getVariants(): Array<Any> = candidates.toTypedArray()
+    override fun resolve(): PsiElement? = candidates.firstOrNull { it.name == element.name }
+    val candidates get() = element.importDeclaration.importedModule?.exportedFixityDeclarations ?: listOf()
     override fun handleElementRename(name: String): PsiElement? {
         val newName = PSPsiFactory(element.project).createOperatorName(name)
             ?: return null
