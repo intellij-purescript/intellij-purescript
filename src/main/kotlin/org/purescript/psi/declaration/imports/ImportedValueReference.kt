4e0a4dd05b7b035ca3288f05d7261085c929b74d
neppord <neppord@gmail.com>
2023-03-01 19:56:13 +0100

    Make reference lazy

--- src/main/kotlin/org/purescript/psi/declaration/imports/ImportedValueReference.kt
+++ src/main/kotlin/org/purescript/psi/declaration/imports/ImportedValueReference.kt
@@ -1,36 +1,34 @@
 package org.purescript.psi.declaration.imports
 
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiElementResolveResult.createResults
 import com.intellij.psi.PsiNamedElement
 import com.intellij.psi.PsiReferenceBase
-import com.intellij.psi.ResolveResult
 import org.purescript.psi.PSPsiFactory
 
-class ImportedValueReference(element: PSImportedValue) : PsiReferenceBase.Poly<PSImportedValue>(
+class ImportedValueReference(element: PSImportedValue) : PsiReferenceBase<PSImportedValue>(
     element,
     element.identifier.textRangeInParent,
     false
 ) {
     override fun getVariants(): Array<PsiNamedElement> =
-        candidates.distinctBy { it.name }
+        candidates.toList().distinctBy { it.name }
             .toTypedArray()
-
-    override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> =
-        createResults(candidates.filter { it.name == element.name })
-
-    private val candidates: List<PsiNamedElement>
+    
+    private val candidates: Sequence<PsiNamedElement>
         get() {
             val module = element.importDeclaration?.importedModule
-                ?: return emptyList()
-            val candidates = mutableListOf<PsiNamedElement>()
-            candidates.addAll(module.exportedValueDeclarationGroups)
-            candidates.addAll(module.exportedForeignValueDeclarations)
-            for (exportedClassDeclaration in module.exportedClassDeclarations) {
-                candidates.addAll(exportedClassDeclaration.classMembers)
+                ?: return emptySequence()
+            return sequence {
+                yieldAll(module.exportedValueDeclarationGroups)
+                yieldAll(module.exportedForeignValueDeclarations)
+                for (exportedClassDeclaration in module.exportedClassDeclarations) {
+                    yieldAll(exportedClassDeclaration.classMembers.asSequence())
+                }
             }
-            return candidates
         }
+
+    override fun resolve(): PsiElement? = candidates.firstOrNull { it.name == element.name }
+
     override fun handleElementRename(name: String): PsiElement? {
         val newName = PSPsiFactory(element.project).createIdentifier(name)
             ?: return null
