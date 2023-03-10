3be18b217c02b54010ddbad0375b6a64f834b29a
neppord <neppord@gmail.com>
2023-03-10 20:58:32 +0100

    Unseal ExpressionAtom

--- src/main/kotlin/org/purescript/psi/expression/ExpressionAtom.kt
+++ src/main/kotlin/org/purescript/psi/expression/ExpressionAtom.kt
@@ -2,6 +2,6 @@ package org.purescript.psi.expression
 
 import com.intellij.psi.PsiElement
 
-sealed interface ExpressionAtom: PsiElement, Expression {
+interface ExpressionAtom: PsiElement, Expression {
     override fun getAtoms(): Sequence<ExpressionAtom> = sequenceOf(this)
 }
