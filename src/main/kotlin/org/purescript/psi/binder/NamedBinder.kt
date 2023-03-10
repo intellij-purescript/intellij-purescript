df6b86acf2faf0c724f743dc645892f4eefaf29f
neppord <neppord@gmail.com>
2023-03-10 15:58:55 +0100

    Add doc comment

--- src/main/kotlin/org/purescript/psi/binder/NamedBinder.kt
+++ src/main/kotlin/org/purescript/psi/binder/NamedBinder.kt
@@ -2,4 +2,11 @@ package org.purescript.psi.binder
 
 import com.intellij.lang.ASTNode
 
+/**
+ * The node `box@(Box a)` in the code
+ * 
+ * ```purescript
+ * f box@(Box a) = a
+ * ```
+ */
 class NamedBinder(node: ASTNode) : Binder(node)
