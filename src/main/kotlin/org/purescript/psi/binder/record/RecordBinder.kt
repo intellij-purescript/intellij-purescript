02553c9c38069f1ce9107b9edd15b776973cac7f
neppord <neppord@gmail.com>
2023-03-10 16:07:25 +0100

    Add doc comment

--- src/main/kotlin/org/purescript/psi/binder/record/RecordBinder.kt
+++ src/main/kotlin/org/purescript/psi/binder/record/RecordBinder.kt
@@ -4,6 +4,10 @@ import com.intellij.lang.ASTNode
 import org.purescript.psi.binder.Binder
 
 /**
- * 
+ * The node `{a: 1, b}` in the code
+ *
+ * ```purescript
+ * f {a: 1, b} = 1
+ * ```
  */
 class RecordBinder(node: ASTNode) : Binder(node)
