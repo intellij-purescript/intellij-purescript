8d23faca364651639afafb5e787d3198ddeeba98
neppord <neppord@gmail.com>
2023-03-10 15:31:36 +0100

    Remove unused member

--- src/main/kotlin/org/purescript/psi/binder/record/RecordLabelExprBinder.kt
+++ src/main/kotlin/org/purescript/psi/binder/record/RecordLabelExprBinder.kt
@@ -1,9 +1,6 @@
 package org.purescript.psi.binder.record
 
 import com.intellij.lang.ASTNode
-import com.intellij.psi.util.childrenOfType
 import org.purescript.psi.binder.Binder
 
-class RecordLabelExprBinder(node: ASTNode) : Binder(node) {
-    val binderAtomChildren get() = childrenOfType<Binder>() 
-}
+class RecordLabelExprBinder(node: ASTNode) : Binder(node)
