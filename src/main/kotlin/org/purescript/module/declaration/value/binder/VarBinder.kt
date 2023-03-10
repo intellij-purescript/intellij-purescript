b361ef4282709af8220bce7fe68250d74d090f82
neppord <neppord@gmail.com>
2023-03-10 22:52:48 +0100

    Narrow use scope for var binding

--- src/main/kotlin/org/purescript/module/declaration/value/binder/VarBinder.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/binder/VarBinder.kt
@@ -6,8 +6,13 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.psi.search.LocalSearchScope
 import com.intellij.psi.search.SearchScope
-import org.purescript.psi.PSPsiFactory
+import com.intellij.psi.util.parentOfType
+import com.intellij.refactoring.suggested.startOffset
+import org.purescript.module.declaration.value.ValueDeclarationGroup
+import org.purescript.module.declaration.value.expression.dostmt.PSDoBlock
+import org.purescript.module.declaration.value.expression.namespace.PSLet
 import org.purescript.name.PSIdentifier
+import org.purescript.psi.PSPsiFactory
 
 /**
  * The node `a` in the code
@@ -18,7 +23,14 @@ import org.purescript.name.PSIdentifier
 class VarBinder(node: ASTNode) : Binder(node), PsiNameIdentifierOwner {
     override fun getName(): String = nameIdentifier.name
     override fun getNameIdentifier() = findChildByClass(PSIdentifier::class.java)!!
-    override fun getUseScope(): SearchScope = LocalSearchScope(containingFile)
+    override fun getUseScope(): SearchScope {
+        val scope = listOfNotNull(
+            parentOfType<ValueDeclarationGroup>(),
+            parentOfType<PSLet>(),
+            parentOfType<PSDoBlock>()
+        ).minByOrNull { it.startOffset } ?: containingFile
+        return LocalSearchScope(scope)
+    }
     override fun setName(name: String): PsiElement? {
         val factory = project.service<PSPsiFactory>()
         val newName = factory.createIdentifier(name) ?: return null
