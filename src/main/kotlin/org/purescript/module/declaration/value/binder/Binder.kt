c15d7238b3ad3a3f4b8f3fbdcdb8fa31edafe9de
neppord <neppord@gmail.com>
2023-03-11 10:45:48 +0100

    Simplify and move getUseScope to Binder from VarBinder using ValueNamespace

--- src/main/kotlin/org/purescript/module/declaration/value/binder/Binder.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/binder/Binder.kt
@@ -2,7 +2,11 @@ package org.purescript.module.declaration.value.binder
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SearchScope
 import com.intellij.psi.util.childrenOfType
+import com.intellij.psi.util.parentOfType
+import org.purescript.module.declaration.value.ValueNamespace
 import org.purescript.psi.PSPsiElement
 
 open class Binder(node: ASTNode) : PSPsiElement(node) {
@@ -10,4 +14,6 @@ open class Binder(node: ASTNode) : PSPsiElement(node) {
         binderChildren.flatMap { it.descendantBinders } + listOf(this)
     val binderChildren get() = childrenOfType<Binder>()
     val namedDescendant get() = descendantBinders.filterIsInstance<PsiNamedElement>()
+    override fun getUseScope(): SearchScope =
+        LocalSearchScope(parentOfType<ValueNamespace>() ?: containingFile)
 }
--- src/main/kotlin/org/purescript/module/declaration/value/binder/VarBinder.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/binder/VarBinder.kt
@@ -4,13 +4,6 @@ import com.intellij.lang.ASTNode
 import com.intellij.openapi.components.service
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
-import com.intellij.psi.search.LocalSearchScope
-import com.intellij.psi.search.SearchScope
-import com.intellij.psi.util.parentOfType
-import com.intellij.refactoring.suggested.startOffset
-import org.purescript.module.declaration.value.ValueDeclarationGroup
-import org.purescript.module.declaration.value.expression.dostmt.PSDoBlock
-import org.purescript.module.declaration.value.expression.namespace.PSLet
 import org.purescript.name.PSIdentifier
 import org.purescript.psi.PSPsiFactory
 
@@ -23,14 +16,6 @@ import org.purescript.psi.PSPsiFactory
 class VarBinder(node: ASTNode) : Binder(node), PsiNameIdentifierOwner {
     override fun getName(): String = nameIdentifier.name
     override fun getNameIdentifier() = findChildByClass(PSIdentifier::class.java)!!
-    override fun getUseScope(): SearchScope {
-        val scope = listOfNotNull(
-            parentOfType<ValueDeclarationGroup>(),
-            parentOfType<PSLet>(),
-            parentOfType<PSDoBlock>()
-        ).minByOrNull { it.startOffset } ?: containingFile
-        return LocalSearchScope(scope)
-    }
     override fun setName(name: String): PsiElement? {
         val factory = project.service<PSPsiFactory>()
         val newName = factory.createIdentifier(name) ?: return null
