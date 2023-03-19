16be7bf87cd77a723297301d2a4e06d58408bf5e
neppord <neppord@gmail.com>
2023-03-19 18:13:28 +0100

    Fix bug due to missing get()

--- src/main/kotlin/org/purescript/module/declaration/value/parameters/Parameters.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/parameters/Parameters.kt
@@ -6,7 +6,7 @@ import org.purescript.module.declaration.value.binder.VarBinder
 import org.purescript.psi.PSPsiElement
 
 class Parameters(node: ASTNode) : PSPsiElement(node) {
-    val valueNames = parameterBinders.asSequence().flatMap { it.namedDescendant }
+    val valueNames get() = parameterBinders.asSequence().flatMap { it.namedDescendant }
     val parameterBinders get() = parameters.flatMap { it.parameterBinders }
     val varBinderParameters get() = parameterBinders.filterIsInstance<VarBinder>()
     val parameters get() = childrenOfType<Parameter>()
