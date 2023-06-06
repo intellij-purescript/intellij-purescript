8c5ce6a5395954b798d7b2d017a5634538a11622
neppord <neppord@gmail.com>
2023-06-06 19:23:04 +0200

    Typecheck forall

--- src/main/kotlin/org/purescript/module/declaration/type/type/ForAll.kt
+++ src/main/kotlin/org/purescript/module/declaration/type/type/ForAll.kt
@@ -1,11 +1,18 @@
 package org.purescript.module.declaration.type.type
 
 import com.intellij.lang.ASTNode
+import org.purescript.module.declaration.type.PSTypeVarName
 import org.purescript.psi.PSPsiElement
 import org.purescript.typechecker.TypeCheckerType
 
 class ForAll(node: ASTNode) : PSPsiElement(node), PSType {
+    private val type get() = findChildByClass(PSType::class.java)
+    private val typeVars get() = findChildrenByClass(PSTypeVarName::class.java)
     override fun checkType(): TypeCheckerType? {
-        return null
+        return typeVars
+            .mapNotNull { it.name }
+            .fold(type?.checkType() ?: return null) { acc: TypeCheckerType, name: String ->
+                TypeCheckerType.ForAll(name, acc)
+            }
     }
 }
--- src/main/kotlin/org/purescript/typechecker/TypeCheckerType.kt
+++ src/main/kotlin/org/purescript/typechecker/TypeCheckerType.kt
@@ -9,12 +9,13 @@ sealed interface TypeCheckerType {
     data class TypeConstructor(val name: String) : TypeCheckerType
     data class TypeApp(val apply: TypeCheckerType, val to: TypeCheckerType) : TypeCheckerType
     data class Row(val labels: List<Pair<String, TypeCheckerType?>>) : TypeCheckerType
+    // ForAll a TypeVarVisibility Text (Maybe (Type a)) (Type a) (Maybe SkolemScope)
+    data class ForAll(val name: String, val scope: TypeCheckerType) : TypeCheckerType
     /*
 data class TypeWildCard(val value: WildcardData) : TypeCheckerType {
     interface WildcardData
 }
 data class KindApp(val name: String) : TypeCheckerType
-data class ForAll(val name: String) : TypeCheckerType
 data class ConstrainedTypeCheckerType(val name: String) : TypeCheckerType
 data class Skolem(val name: String) : TypeCheckerType
 data class KindedType(val label: String) : TypeCheckerType
