6c9b6e4daa45b1bbdb23c0dffc86b46d38adb9af
neppord <neppord@gmail.com>
2023-06-09 13:59:11 +0200

    Remove one dependency of PSValue

--- src/main/kotlin/org/purescript/module/declaration/value/expression/namespace/Let.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/expression/namespace/Let.kt
@@ -5,13 +5,12 @@ import com.intellij.psi.util.childrenOfType
 import org.purescript.module.declaration.value.ValueDeclarationGroup
 import org.purescript.module.declaration.value.ValueNamespace
 import org.purescript.module.declaration.value.expression.Expression
-import org.purescript.module.declaration.value.expression.PSValue
 import org.purescript.psi.PSPsiElement
 import org.purescript.typechecker.TypeCheckerType
 
 class Let(node: ASTNode) : PSPsiElement(node), Expression, ValueNamespace {
     val valueDeclarationGroups get() = childrenOfType<ValueDeclarationGroup>().asSequence()
-    val value: PSValue? get() = findChildByClass(PSValue::class.java)
+    val value get() = findChildByClass(Expression::class.java)
     private val binderChildren get() = childrenOfType<LetBinder>().asSequence()
     private val namedBinders get() = binderChildren.flatMap { it.namedBinders }
     override val valueNames get() = valueDeclarationGroups + namedBinders
