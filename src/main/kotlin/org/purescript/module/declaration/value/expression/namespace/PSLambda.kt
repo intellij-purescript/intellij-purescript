e74c151a521642ca8a93340b8be6b1e4092f75d1
neppord <neppord@gmail.com>
2023-06-09 13:41:45 +0200

    Remove dependency of PSValue in PSLambda

--- src/main/kotlin/org/purescript/module/declaration/value/expression/namespace/PSLambda.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/expression/namespace/PSLambda.kt
@@ -3,7 +3,6 @@ package org.purescript.module.declaration.value.expression.namespace
 import com.intellij.lang.ASTNode
 import org.purescript.module.declaration.value.ValueNamespace
 import org.purescript.module.declaration.value.expression.Expression
-import org.purescript.module.declaration.value.expression.PSValue
 import org.purescript.module.declaration.value.parameters.Parameters
 import org.purescript.psi.PSPsiElement
 import org.purescript.typechecker.TypeCheckerType
@@ -11,7 +10,7 @@ import org.purescript.typechecker.TypeCheckerType
 class PSLambda(node: ASTNode) : PSPsiElement(node), Expression, ValueNamespace {
     val parameterList get() = findChildByClass(Parameters::class.java)
     val parameters get() = parameterList?.parameters
-    val value: PSValue? get() = findChildByClass(PSValue::class.java)
+    val value get() = findChildByClass(Expression::class.java)
     override val valueNames get() = parameterList?.valueNames ?: emptySequence()
     override fun checkType(): TypeCheckerType? = null
 }
