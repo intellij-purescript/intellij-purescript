3e29fe5746a5e6282e6682fa2feb4864cc33044f
neppord <neppord@gmail.com>
2023-06-09 13:50:31 +0200

    Remove one dependency of PSValue

--- src/main/kotlin/org/purescript/module/declaration/value/expression/literals/PSArrayLiteral.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/expression/literals/PSArrayLiteral.kt
@@ -1,14 +1,14 @@
 package org.purescript.module.declaration.value.expression.literals
 
 import com.intellij.lang.ASTNode
+import org.purescript.module.declaration.value.expression.Expression
 import org.purescript.module.declaration.value.expression.ExpressionAtom
-import org.purescript.module.declaration.value.expression.PSValue
 import org.purescript.psi.PSPsiElement
 import org.purescript.typechecker.TypeCheckable
 import org.purescript.typechecker.TypeCheckerType
 
 class PSArrayLiteral(node: ASTNode) : PSPsiElement(node), ExpressionAtom {
-    val values get() = findChildrenByClass(PSValue::class.java)
+    val values get() = findChildrenByClass(Expression::class.java)
 
     override fun checkType(): TypeCheckerType = TypeCheckerType.TypeApp(
         TypeCheckerType.TypeConstructor("Prim.Array"),
