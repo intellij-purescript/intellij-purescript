891a2a51fc1b4e36875cc88577382cbafe69435a
neppord <neppord@gmail.com>
2023-06-23 09:24:42 +0200

    Move call into each branch of when and simplify

--- src/main/kotlin/org/purescript/module/declaration/value/expression/identifier/ExpressionIdentifierReference.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/expression/identifier/ExpressionIdentifierReference.kt
@@ -44,11 +44,10 @@ class ExpressionIdentifierReference(expressionIdentifier: PSExpressionIdentifier
             null -> element
                 .parentsOfType<ValueNamespace>(withSelf = false)
                 .flatMap { it.valueNames }
-                .filter { it.containingFile == element.containingFile }
+                .filter { it.containingFile == element.containingFile }.firstOrNull { it.name == name }
 
-            else -> emptySequence()
-        }.firstOrNull { it.name == name }
-            ?: importedCandidates.firstOrNull { it.name == name }
+            else -> null
+        } ?: importedCandidates.firstOrNull { it.name == name }
     }
 
     private fun getImportedCandidates(): Sequence<PsiNamedElement> {
