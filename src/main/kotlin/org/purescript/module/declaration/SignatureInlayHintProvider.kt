fba4c262d65c08b1205221c8a3c8860bf4e53537
neppord <neppord@gmail.com>
2023-06-25 12:12:39 +0200

    Make typehints for records collapsable

--- src/main/kotlin/org/purescript/module/declaration/SignatureInlayHintProvider.kt
+++ src/main/kotlin/org/purescript/module/declaration/SignatureInlayHintProvider.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.refactoring.suggested.endOffset
+import org.purescript.inference.InferType
 import org.purescript.inference.RecursiveTypeException
 import org.purescript.inference.inferType
 import org.purescript.module.declaration.value.ValueDeclarationGroup
@@ -15,8 +16,54 @@ class SignatureInlayHintProvider : InlayHintsProvider {
             override fun collectFromElement(element: PsiElement, sink: InlayTreeSink) {
                 when (element) {
                     is ValueDeclarationGroup -> if (element.signature == null) {
+                        fun PresentationTreeBuilder.pprint(type: InferType) {
+                            when (type) {
+                                is InferType.App -> {
+                                    if (type.f == InferType.Record) {
+                                        text("{")
+                                        collapsibleList(CollapseState.Collapsed,
+                                            {
+                                                toggleButton {
+                                                    var first = true
+                                                    for (label in (type.on as InferType.Row).mergedLabels()) {
+                                                        if (!first) text(", ")
+                                                        first = false
+                                                        text("${label.first} :: ")
+                                                        pprint(label.second)
+                                                    }
+                                                }
+                                            },
+                                            {toggleButton { text("...") }}
+                                        )
+                                        text("}")
+                                    } else {
+                                        text("${type.f} ")
+                                        pprint(type.on)
+                                    }    
+                                }
+                                is InferType.Row -> {
+                                    collapsibleList(CollapseState.Collapsed,
+                                        {
+                                            text("(")
+                                            var first = true
+                                            for (label in type.mergedLabels()) {
+                                                if (!first) text(", ")
+                                                first = false
+                                                text("${label.first} :: ")
+                                                pprint(label.second)
+                                            }
+                                            text(")")
+                                        },
+                                        { toggleButton { text("...") } }
+                                    )
+                                }
+
+                                else ->
+                                    text("$type")
+                            }
+                        }
                         try {
-                            val tooltip = ":: ${element.inferType()}"
+                            val type = element.inferType()
                             sink.addPresentation(
                                 InlineInlayPosition(
                                     element.nameIdentifier.endOffset,
@@ -26,11 +73,12 @@ class SignatureInlayHintProvider : InlayHintsProvider {
                                 null,
                                 true,
                             ) {
-                                text(tooltip)
+                                text(":: ")
+                                this.pprint(type)
                             }
-                        } catch (e : NotImplementedError) {
+                        } catch (e: NotImplementedError) {
                             return
-                        } catch (e : RecursiveTypeException) {
+                        } catch (e: RecursiveTypeException) {
                             return
                         }
                     }
