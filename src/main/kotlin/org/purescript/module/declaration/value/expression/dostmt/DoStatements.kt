24e23c03c3a22793c7ca6c33afa40d9a10854f94
neppord <neppord@gmail.com>
2023-07-08 17:29:50 +0200

    Fix go to usage regression in DoStatements

--- src/main/kotlin/org/purescript/module/declaration/value/expression/dostmt/DoStatements.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/expression/dostmt/DoStatements.kt
@@ -1,6 +1,7 @@
 package org.purescript.module.declaration.value.expression.dostmt
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNamedElement
 import com.intellij.psi.util.childrenOfType
 import org.purescript.module.declaration.value.ValueNamespace
@@ -10,12 +11,15 @@ class DoStatements(node: ASTNode) : PSPsiElement(node), DoStatement, ValueNamesp
     private val children = childrenOfType<DoStatement>()
     override val binders get() = children.flatMap { it.binders }
     override val statements get() = children.flatMap { it.statements }
+    override val scopes: Array<PsiElement> get() = (parent as? DoStatements)?.scopes ?: arrayOf(this)
     override val valueNames: Sequence<PsiNamedElement>
-        get() = children.asSequence().flatMap { when(it) {
-            is DoStatements -> it.valueNames
-            is PSDoNotationLet -> it.valueNames
-            is PSDoNotationBind -> it.binders.filterIsInstance<PsiNamedElement>().asSequence()
-            else -> emptySequence()
-        } }
+        get() = children.asSequence().flatMap {
+            when (it) {
+                is DoStatements -> it.valueNames
+                is PSDoNotationLet -> it.valueNames
+                is PSDoNotationBind -> it.binders.filterIsInstance<PsiNamedElement>().asSequence()
+                else -> emptySequence()
+            }
+        }
 
 }
