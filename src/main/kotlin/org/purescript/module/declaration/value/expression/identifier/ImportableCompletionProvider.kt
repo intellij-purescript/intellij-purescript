49596f58897238211925a39cc9592fb91bcfda42
Mark Eibes <mark.eibes@gmail.com>
2023-11-11 16:53:13 +0100

    Autocomplete imports under namespace

--- src/main/kotlin/org/purescript/module/declaration/value/expression/identifier/ImportableCompletionProvider.kt
+++ src/main/kotlin/org/purescript/module/declaration/value/expression/identifier/ImportableCompletionProvider.kt
@@ -26,9 +26,9 @@ import org.purescript.run.spago.Spago
 
 class ImportableCompletionProvider : CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
-        parameters: CompletionParameters,
-        context: ProcessingContext,
-        result: CompletionResultSet
+            parameters: CompletionParameters,
+            context: ProcessingContext,
+            result: CompletionResultSet
     ) {
         if (parameters.isExtendedCompletion) {
             addInstallCompletions(parameters, result)
@@ -48,68 +48,91 @@ class ImportableCompletionProvider : CompletionProvider<CompletionParameters>() 
             for ((packageName, import) in data) {
                 val moduleName = import.moduleName
                 result.addElement(LookupElementBuilder
-                    .create(packageName to moduleName, name)
-                    .withIcon(AllIcons.Actions.Install)
-                    .withTailText("($moduleName)")
-                    .appendTailText("($packageName)", true)
-                    .withInsertHandler { context, _ ->
-                        val commandLine = project.service<Spago>().commandLine
-                            .withParameters("install", packageName)
-                        val qualifiedImport = import.withAlias(qualifiedName)
-                        val module = (context.file as PSFile).module
-                        runBackgroundableTask("Installing package: $packageName", project) {
-                            ExecUtil.execAndGetOutput(commandLine)
-                            VirtualFileManager.getInstance().asyncRefresh {}
-                        }
-                        executeCommand(project, "Import") {
-                            runWriteAction {
-                                module?.addImportDeclaration(qualifiedImport)
+                        .create(packageName to moduleName, name)
+                        .withIcon(AllIcons.Actions.Install)
+                        .withTailText("($moduleName)")
+                        .appendTailText("($packageName)", true)
+                        .withInsertHandler { context, _ ->
+                            val commandLine = project.service<Spago>().commandLine
+                                    .withParameters("install", packageName)
+                            val qualifiedImport = import.withAlias(qualifiedName)
+                            val module = (context.file as PSFile).module
+                            runBackgroundableTask("Installing package: $packageName", project) {
+                                ExecUtil.execAndGetOutput(commandLine)
+                                VirtualFileManager.getInstance().asyncRefresh {}
                             }
-                        }
-                    })
+                            executeCommand(project, "Import") {
+                                runWriteAction {
+                                    module?.addImportDeclaration(qualifiedImport)
+                                }
+                            }
+                        })
             }
         }
     }
 
     private fun completionsOfNamespace(parameters: CompletionParameters, result: CompletionResultSet) {
         val localElement = parameters.position.parentOfType<Qualified>(true) ?: return
-        if ((localElement as? Qualified)?.qualifierName != null) return
-        val module = (localElement  as? PSPsiElement)?.module ?: return
-        val nameSpaces = module.cache.importsByAlias.keys.filterNotNull()
-        result.addAllElements(
-            nameSpaces
-                .filter { result.prefixMatcher.prefixMatches(it) }
-                .map {
-                LookupElementBuilder.create("$it.")
-            }
-        )
-        for (nameSpace in nameSpaces) {
-            val imports = module.cache.importsByAlias[nameSpace] ?: continue
-            for (import in imports) {
-                val items = import.importedValueNames + import.importedConstructors
+        when (val qualifier = (localElement as? Qualified)?.qualifierName) {
+            null -> {
+                val module = (localElement as? PSPsiElement)?.module ?: return
+                val nameSpaces = module.cache.importsByAlias.keys.filterNotNull()
                 result.addAllElements(
-                    items.map { LookupElementBuilder
-                        .create(it, "$nameSpace.${it.name}")
-                        .withIcon(it.getIcon(0))
-                        .withTypeText((it as? org.purescript.module.declaration.Importable)?.type?.text)
-                        .withTailText("(${import.moduleNameName})")
-                        .let { PrioritizedLookupElement.withPriority(it, 1.0) }
-                    }
+                        nameSpaces
+                                .filter { result.prefixMatcher.prefixMatches(it) }
+                                .map {
+                                    LookupElementBuilder.create("$it.")
+                                }
                 )
+                for (nameSpace in nameSpaces) {
+                    val imports = module.cache.importsByAlias[nameSpace] ?: continue
+                    for (import in imports) {
+                        val items = import.importedValueNames + import.importedConstructors
+                        result.addAllElements(
+                                items.map {
+                                    LookupElementBuilder
+                                            .create(it, "$nameSpace.${it.name}")
+                                            .withIcon(it.getIcon(0))
+                                            .withTypeText((it as? org.purescript.module.declaration.Importable)?.type?.text)
+                                            .withTailText("(${import.moduleNameName})")
+                                            .let { PrioritizedLookupElement.withPriority(it, 1.0) }
+                                }
+                        )
+                    }
+                }
+            }
+
+            else -> {
+                val module = (localElement as? PSPsiElement)?.module ?: return
+                val imports = module.cache.importsByAlias[qualifier] ?: return
+                for (import in imports) {
+                    val items = import.importedValueNames + import.importedConstructors
+                    result.addAllElements(
+                            items.map {
+                                LookupElementBuilder
+                                        .create(it, "${it.name}")
+                                        .withIcon(it.getIcon(0))
+                                        .withTypeText((it as? org.purescript.module.declaration.Importable)?.type?.text)
+                                        .withTailText("(${import.moduleNameName})")
+                                        .let { PrioritizedLookupElement.withPriority(it, 1.0) }
+                            }
+                    )
+                }
             }
         }
     }
+
     private fun completionsFromIndex(parameters: CompletionParameters, result: CompletionResultSet) {
         val localElement = parameters.position
         val qualifiedName = localElement.parentOfType<Qualified>()?.qualifierName
         val improvedResult = if (qualifiedName != null) {
-            val qualifiedMather =  result.prefixMatcher.cloneWithPrefix(qualifiedName)
+            val qualifiedMather = result.prefixMatcher.cloneWithPrefix(qualifiedName)
             result
-                .withRelevanceSorter(CompletionSorter.emptySorter().weigh(object : LookupElementWeigher("qualifier") {
-                    override fun weigh(element: LookupElement): Comparable<Nothing> {
-                        return - element.allLookupStrings.count {qualifiedMather.prefixMatches(it)}
-                    }
-                }))
+                    .withRelevanceSorter(CompletionSorter.emptySorter().weigh(object : LookupElementWeigher("qualifier") {
+                        override fun weigh(element: LookupElement): Comparable<Nothing> {
+                            return -element.allLookupStrings.count { qualifiedMather.prefixMatches(it) }
+                        }
+                    }))
         } else {
             result
         }
@@ -123,37 +146,37 @@ class ImportableCompletionProvider : CompletionProvider<CompletionParameters>() 
             if (!improvedResult.prefixMatcher.prefixMatches(name)) continue
             val elements = index.get(name, project, scope)
             val elementBuilders = elements
-                .filter { parameters.originalFile != it.containingFile }
-                .mapNotNull { lookupElementBuilder(it, qualifiedName, project) }
+                    .filter { parameters.originalFile != it.containingFile }
+                    .mapNotNull { lookupElementBuilder(it, qualifiedName, project) }
             improvedResult.addAllElements(elementBuilders)
         }
     }
 
     private fun lookupElementBuilder(it: org.purescript.module.declaration.Importable, qualifiedName: String?, project: Project): LookupElementBuilder {
-        val import = it.asImport()?.withAlias(qualifiedName) 
-            ?: error("Importable was not importable")
+        val import = it.asImport()?.withAlias(qualifiedName)
+                ?: error("Importable was not importable")
         val modulePath = import.moduleName.split('.')
 
         val lookupElement = LookupElementBuilder
-            .createWithIcon(it)
-            .withPresentableText(it.name!!)
-            .withLookupString("${modulePath.joinToString("")}.${it.name}")
-            .withLookupString("${modulePath.reversed().joinToString("")}.${it.name}")
-            .withTypeText(it.type?.text)
-            .withTailText("(${import.moduleName})")
-            .withInsertHandler { context, item ->
-                val module = (context.file as PSFile).module
-                executeCommand(project, "Import") {
-                    runWriteAction {
-                        module?.addImportDeclaration(import)
+                .createWithIcon(it)
+                .withPresentableText(it.name!!)
+                .withLookupString("${modulePath.joinToString("")}.${it.name}")
+                .withLookupString("${modulePath.reversed().joinToString("")}.${it.name}")
+                .withTypeText(it.type?.text)
+                .withTailText("(${import.moduleName})")
+                .withInsertHandler { context, item ->
+                    val module = (context.file as PSFile).module
+                    executeCommand(project, "Import") {
+                        runWriteAction {
+                            module?.addImportDeclaration(import)
+                        }
                     }
                 }
-            }
         val file = it.containingFile.virtualFile
         val libraries = project.service<Spago>().libraries
         return libraries.find {
             it.sourceRoots.any { it.toNioPath().isAncestor(file.toNioPath()) }
         }?.packageName?.let { lookupElement.appendTailText("($it)", true) }
-            ?: lookupElement
+                ?: lookupElement
     }
 }
