ac03aaed3c3602a400d8c16cca003d84aefdefe9
Samuel Ytterbrink <neppord@gmail.com>
2024-05-06 19:53:27 +0200

    Update gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     java
     kotlin("jvm") version "1.9.21"
     kotlin("plugin.serialization") version "1.9.21"
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.3"
     id("org.jetbrains.grammarkit") version "2022.3.2"
 }
 
